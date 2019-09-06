b184d41caa9b9dceb3e3451402ecaf8ea1c4cda0
rickymcallister45 <rickymcallister45@gmail.com>
2019-09-06 07:04:54 -0400

    Update toDo.sql

--- app/toDo/toDo.sql
+++ app/toDo/toDo.sql
@@ -1,1 +1,9 @@
 /*  things needed ---id , title , status , ipAddress  */
+
+CREATE TABLE toDo 
+  (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    status INT,
+    ipAddress INT UNSIGNED
+    );
