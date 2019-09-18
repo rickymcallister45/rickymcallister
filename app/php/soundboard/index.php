6bf591fe54ee2fc76f99231360b1b5ea519efb61
Ricky McAllister <rickymcallister45@gmail.com>
2019-09-18 00:57:31 -0400

    Update index.php

--- app/php/soundboard/index.php
+++ app/php/soundboard/index.php
@@ -3,11 +3,51 @@ $path    = '../../../files/mp3';
 $files = scandir($path);
 $files = array_diff(scandir($path), array('.', '..'));
 
-foreach($sound as $files){
+print_r($files);
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>rickyMcAllister</title>
+    <link rel="stylesheet" type="text/css" href="soundboard.css">
+    
+</head>
+<body>
+  <h1>rickyMcAllister</h1>
+<div id="soundboard">
+  
+  foreach($sound as $files){
   echo "<ul class='sounds'>.$sound.</ul>";
 }
+  
+  
+  
+  
+  </div>
+
+</body>
+<footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script type="text/javascript">
+      $('.sounds').click(function() {
+        var name = $(this).text();
+        var audio = new Audio('../../../files/mp3/' + name + ".mp3"');
+        console.log(name);
+        audio.play();
+        
+            });
+    
+    </script>
+</footer>
+</html>
 
 
 
-print_r($files);
+
+
+
+
+
+
 ?>
