558a77f5e2f9ce332a1d9ee9843451dcd0480c43
Ricky McAllister <rickymcallister45@gmail.com>
2019-09-18 01:04:18 -0400

    Delete dbh.inc.php

--- app/php/dbh.inc.php
+++
@@ -1,8 +0,0 @@
-<?php	
-
-$dbServername ="localhost";
-$dbUsername = "username";
-$dbPassword = "Swordfish";
-$dbName = "loginsystem";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
