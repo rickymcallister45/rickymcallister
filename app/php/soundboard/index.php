7786c95bd64dcf4b5f220497cc0e9039d6af4770
Ricky McAllister <rickymcallister45@gmail.com>
2019-09-19 17:20:55 -0400

    Update index.php

--- app/php/soundboard/index.php
+++ app/php/soundboard/index.php
@@ -3,7 +3,7 @@ $path    = '../../../files/mp3';
 $files = scandir($path);
 $files = array_diff(scandir($path), array('.', '..'));
 
-print_r($files.</br>);
+print_r($files);
 ?>
 <!DOCTYPE html>
 <html>
