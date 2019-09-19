9a236855d013d773dad04db91a42c23f24463335
Ricky McAllister <rickymcallister45@gmail.com>
2019-09-19 17:35:10 -0400

    Create soundboard.js

---
+++ app/php/soundboard/soundboard.js
@@ -0,0 +1,7 @@
+$('.sounds').click(function() {
+        var name = $(this).text();
+        var audio = new Audio('../../../files/mp3/' + name + ".mp3");
+        console.log(name);
+        audio.play();
+        
+            });
