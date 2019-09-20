f2b7c0753e773feb1358f7f2c900048305bfd1f9
Ricky McAllister <rickymcallister45@gmail.com>
2019-09-19 23:32:12 -0400

    Update and rename index.html to index.php

---
+++ app/soundboard/index.php
@@ -0,0 +1,36 @@
+<?php
+$path    = '../../../files/mp3';
+$files = scandir($path);
+$files = array_diff(scandir($path), array('.', '..'));
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>rickyMcAllister</title>
+    <link rel="stylesheet" type="text/css" href="soundboard.css">
+    
+</head>
+<body>
+  <h1>rickyMcAllister</h1>
+<div id="soundboard">
+  
+  <?php
+  for($i=0; $i< count($files); $i++){
+  echo "<ul class='sounds'>".$files[$i]."</ul>";
+}
+  ?>
+  
+  
+  
+  </div>
+
+</body>
+<footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+      <script src="soundboard.js"></script>
+    
+    </script>
+</footer>
+</html>
+
