54ca04b75a55324dc10419fb5867b835c8047611
Ricky McAllister <rickymcallister45@gmail.com>
2019-09-19 23:34:56 -0400

    Update soundboard.js

--- app/soundboard/soundboard.js
+++ app/soundboard/soundboard.js
@@ -1,1 +1,8 @@
+$('.sounds').click(function() {
+        var name = $(this).text();
+        var audio = new Audio('../../files/mp3/' + name );
+        console.log(name);
+        audio.play();
+        
+            });
 
