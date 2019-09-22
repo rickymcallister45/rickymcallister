5d644c8ddd6a16a17d381c11ba3d32a78feff11f
Ricky McAllister <rickymcallister45@gmail.com>
2019-09-22 18:30:41 -0400

    Update index.php

--- app/php/usedForTesting/database/index.php
+++ app/php/usedForTesting/database/index.php
@@ -4,20 +4,6 @@ echo $dateAndTime = date('Y-m-d H:i:s');
 
 require_once('./connect.db.php');
 
-$sql = "INSERT INTO `toDo` SET `title`='testing todo insert',`time`='{$dateAndTime}',`ip`='{$IP_Adress}'";
-
-$db->query($sql);
-if($db->error){
-  echo $db->error;
-}else{
-  echo "Submitted from  ".$IP_Adress;
-}
-
-?>
-
-/*
-
-  9:05 //delete lines 7 through here 
 
 if(!empty($_POST['submit'])) {
     $sql = "INSERT INTO `toDo` SET `title`='{$_POST['title']}', `time`='{$dateAndTime}', `ip`='$IP_Adress'";
@@ -31,7 +17,19 @@ if($db->error){
 
 
 }
+/*used for testing
+$sql = "INSERT INTO `toDo` SET `title`='testing todo insert',`time`='{$dateAndTime}',`ip`='{$IP_Adress}'";
 
+$db->query($sql);
+if($db->error){
+  echo $db->error;
+}else{
+  echo "Submitted from  ".$IP_Adress;
+}
+
+?>
+
+/
 ?>
 
 <form action="" method="POST">
