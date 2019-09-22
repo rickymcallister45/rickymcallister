742f67794445d2f4a5de4dd37450fd29737cdb2e
Ricky McAllister <rickymcallister45@gmail.com>
2019-09-22 02:56:51 -0400

    Update insert.php

--- app/php/usedForTesting/database/insert.php
+++ app/php/usedForTesting/database/insert.php
@@ -1,16 +1,17 @@
 <?php 
+$IP_Adress = $_SERVER[REMOTE_ADDR];
 
 echo $dateAndTime = date('Y-m-d H:i:s');
 
 require_once('./connect.php');
 
-$sql = "INSERT INTO `message` SET `userName`='RickyMcAllister',`postTime`='{$dateAndTime}',`message`='Content of message'";
+$sql = "INSERT INTO `toDo` SET `Title`='testing todo insert',`time`='{$dateAndTime}',`ip`='{$IP_Adress}'";
 
 $db->query($sql);
 if($db->error){
   echo $db->error;
 }else{
-  echo "Submitted";
+  echo "Submitted from  ".$IP_Adress;
 }
 
 ?>
