aa6c75f9c129557b773a7ad0270bfaff73172052
Ricky McAllister <rickymcallister45@gmail.com>
2019-09-22 18:43:55 -0400

    Update index.php

--- app/php/usedForTesting/database/index.php
+++ app/php/usedForTesting/database/index.php
@@ -18,35 +18,9 @@ if($db->error){
 }
 
 ?>
-<form action="" method="POST">
-  TASK: <input name='title' type='text' value=''/><br/>
-  <input name='submit' type='submit' value='submit'/>
-</form>
-      
-
-
-
-
-      
-/*used for testing
-$sql = "INSERT INTO `toDo` SET `title`='testing todo insert',`time`='{$dateAndTime}',`ip`='{$IP_Adress}'";
-
-$db->query($sql);
-if($db->error){
-  echo $db->error;
-}else{
-  echo "Submitted from  ".$IP_Adress;
-}
-
-?>
-
-/
-?>
 
 <form action="" method="POST">
   TASK: <input name='title' type='text' value=''/><br/>
   <input name='submit' type='submit' value='submit'/>
 </form>
-
-
-*/
+  
