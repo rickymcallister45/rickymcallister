b24372a51578fdacb5953317afd6bf7c8f1935f3
Ricky McAllister <rickymcallister45@gmail.com>
2019-09-22 00:20:30 -0400

    Update vanillaBackground.js

--- app/desktop/js/vanillaBackground.js
+++ app/desktop/js/vanillaBackground.js
@@ -1,14 +1,19 @@
-
 function background(){
 var request = new XMLHttpRequest();
-var url = 'https://www.reddit.com/r/roomporn.json';
+var url = 'https://www.reddit.com/r/';
+var differnetSites = ['roomporn', 'CozyPlaces', 'MostBeautiful', 'pic', 'AbandonedPorn', 'EarthPorn', 'SkyPorn', 'astrophotography', 'spaceporn', 'itookapicture', 'Eyebleach'];
+
+var randomNumberToSelectSite = Math.floor(Math.random() * 11);
+var siteTheBackroundImageIsComingFrom = differnetSites[randomNumberToSelectSite];
+console.log('siteTheBackroundImageIsComingFrom = ' + url + siteTheBackroundImageIsComingFrom);
+
+
 var backgroundURL; 
-    request.open('GET', url, true);
+    request.open('GET', url + siteTheBackroundImageIsComingFrom + '.json', true);
     request.onload = function() {
           var data = JSON.parse(request.responseText);
           var i = Math.floor(Math.random() * 11) + 1;
           backgroundURL = (data.data.children[i].data.url);
-          console.log(backgroundURL);
           document.body.style.backgroundImage = "url(" + backgroundURL + ")";
           
       }
