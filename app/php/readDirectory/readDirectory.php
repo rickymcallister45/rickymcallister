3509af212e2b659f25481acc7af7a1ec7de4a072
Ricky McAllister <rickymcallister45@gmail.com>
2019-09-22 01:27:41 -0400

    Update readDirectory.php

--- app/php/readDirectory/readDirectory.php
+++ app/php/readDirectory/readDirectory.php
@@ -1,6 +1,12 @@
 <?php
-$path    = '../../../files/mp3';
-$files = scandir($path);
-$files = array_diff(scandir($path), array('.', '..'));
-print_r($files);
+  if (isset($_POST['submit'])) {
+	    if(empty($_POST['input'])) {
+		      $errors = "You must fill in input";
+	    } else {
+		    $pathYouAreChecking = $_POST['input'];
+		    $files = scandir($pathYouAreChecking);
+        $files = array_diff(scandir($path), array('.', '..'));
+        print_r($files);
+	}
+}
 ?>
