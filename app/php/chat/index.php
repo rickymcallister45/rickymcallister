540aba838b15dab6ecbc8c3d51b83396a1352920
Ricky McAllister <rickymcallister45@gmail.com>
2019-09-24 00:25:25 -0400

    Update index.php

--- app/php/chat/index.php
+++ app/php/chat/index.php
@@ -2,3 +2,16 @@
 
 
 ?>
+<!doctype html>
+
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+    
+  </head>
+
+  <body>
+    <h1>Hello Plunker!</h1>
+  </body>
+  <script src="script.js"></script>
+</html>
