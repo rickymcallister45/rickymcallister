17963f1cdcda9802ebbee3c007250750cfccc2a8
Ricky McAllister <rickymcallister45@gmail.com>
2019-09-24 00:58:08 -0400

    Update script.js

--- app/php/chat/script.js
+++ app/php/chat/script.js
@@ -1,1 +1,14 @@
 console.log('running');
+
+$('#textArea').keyup(function(event){
+  if(event.which == 13) {
+    $(form).submit();
+    }
+ });
+
+$('form').submit(function(){
+  alert('form is submitted using jquery');
+  
+  return false;
+
+});
