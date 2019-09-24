3063e84689edddce58fca83601946e745314a01f
Ricky McAllister <rickymcallister45@gmail.com>
2019-09-24 01:53:11 -0400

    Update connect.db.php

--- app/php/usedForTesting/database/connect.db.php
+++ app/php/usedForTesting/database/connect.db.php
@@ -6,11 +6,11 @@ $dbPassword = "password";
 $dbName = "test";
 
 $db = @new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
-
+/*
 if($db->connect_error){
   echo $db->connect_error;
 }
 
 echo "Connection Successful";
-
+*/
 ?>
