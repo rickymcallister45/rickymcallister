b804385a7b13c5949a46b51057bfa912f39e12cb
Ricky McAllister <rickymcallister45@gmail.com>
2019-09-25 09:54:49 -0400

    Update index.php

--- app/php/usedForTesting/database/index.php
+++ app/php/usedForTesting/database/index.php
@@ -25,6 +25,7 @@ if($db->error){
 
 <html>
   <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>RickyMcAllister</title>
       <link rel="stylesheet" href="style.css">
     
