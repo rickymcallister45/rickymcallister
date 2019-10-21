c871803e6bc06dedd35b8524912b6abeef6c5f6c
Ricky McAllister <rickymcallister45@gmail.com>
2019-10-21 06:09:04 -0400

    Create index.php

---
+++ app/computerScience/development/Vue/workingWithDatabase/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+  <head>
+  </head>
+  
+  <script type='text/javascript' src='#'></script>
+
+  <body>
+    <div id='app'>
+      <ul id='list' v-if='car_info_set.length'>
+        <li v-for=''>
+        </li>
+      </ul>
+      <p id='err_msg' v-else>{{err_msg}}</p>
+    </div>
+
+  <script type='text/javascript'></script>
+  
+  </body>
+</html>
