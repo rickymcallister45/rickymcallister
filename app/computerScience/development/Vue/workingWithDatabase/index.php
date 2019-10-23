c0a89cc25bcfc3d871dbf50aa7e30797d7abe1ae
Ricky McAllister <rickymcallister45@gmail.com>
2019-10-23 06:40:33 -0400

    Update index.php

--- app/computerScience/development/Vue/workingWithDatabase/index.php
+++ app/computerScience/development/Vue/workingWithDatabase/index.php
@@ -2,18 +2,18 @@
   <head>
   </head>
   
-  <script type='text/javascript' src='#'></script>
-
+  <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/vue/dist/vue.js'></script>
   <body>
     <p>335</p>
     <div id='app'>
       <ul id='list' v-if='car_info_set.length'>
-        <li v-for=''>
+        <li v-for='element in car_info_set'>
+		Brand is <b>{{element.brand}}</b>, model is <b>{{element.model}}</b>,
+		engine is <b>{{element.engin}}</b>, and gearbox is <b>{{element.gearbox}}</b>
         </li>
       </ul>
       <p id='err_msg' v-else>{{err_msg}}</p>
     </div>
-
   <script type='text/javascript' src='js/app.js'></script>
   
   </body>
