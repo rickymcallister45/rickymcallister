4f8ac9507a17aace3b360c1b68a8fbac2ff7a230
Ricky McAllister <rickymcallister45@gmail.com>
2019-10-23 06:41:10 -0400

    Update app.js

--- app/computerScience/development/Vue/workingWithDatabase/js/app.js
+++ app/computerScience/development/Vue/workingWithDatabase/js/app.js
@@ -1,5 +1,5 @@
 var vm = new Vue({
-  el: 'app1,
+  el: '#app',
   data: {
     car_info_set: [
       {brand: 'Toyota', model: 'Camry', engine: 'petrol', gearbox: 'automatic'},
@@ -9,5 +9,27 @@ var vm = new Vue({
       {brand: 'Honda', model: 'Civic', engine: 'petrol', gearbox: 'automatic'},           
     ],
     err_msg: 'Sorry, no car in record',
+    ajax: null,
+    url: 'php/interface.php', 
+  },
+  created: function(){
+	this.ajax = new XMLHttpRequest()
+	this.request()
+	
+},
+  methods: {
+	request: function(){
+		this.ajax.onreadystatechange = this.response
+		this.ajax.open('POST', this.url, true)
+		this.ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+		this.ajax.send('range-all')
+ },
+	response: function(){
+		if(this.ajax.readyState == 4) {
+			if(this.ajax.status == 200) {
+ 				this.err_msg = JSON.parse(this.ajax.responseText)
+   }
   }
+ },	
+  }	
 })
