6662eee249a27c86b864bc2507d1f0dfbfdf30b0
Ricky McAllister <rickymcallister45@gmail.com>
2019-10-23 03:04:23 -0400

    Update app.js

--- app/computerScience/development/Vue/workingWithDatabase/js/app.js
+++ app/computerScience/development/Vue/workingWithDatabase/js/app.js
@@ -1,1 +1,13 @@
-adf
+var vm = new Vue({
+  el: 'app1,
+  data: {
+    car_info_set: [
+      {brand: 'Toyota', model: 'Camry', engine: 'petrol', gearbox: 'automatic'},
+      {brand: 'Nissan', model: 'GT-R', engine: 'petrol', gearbox: 'automatic'},
+      {brand: 'Honda', model: 'NSX', engine: 'petrol', gearbox: 'automatic'},
+      {brand: 'Honda', model: 'CR-V', engine: 'petrol', gearbox: 'automatic'},
+      {brand: 'Honda', model: 'Civic', engine: 'petrol', gearbox: 'automatic'},           
+    ],
+    err_msg: 'Sorry, no car in record',
+  }
+})
