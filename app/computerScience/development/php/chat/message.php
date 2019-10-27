02e1830edba1ea8d5c0796eeae0541b372eec6c3
Ricky McAllister <rickymcallister45@gmail.com>
2019-10-27 01:17:56 -0400

    Create message.php

---
+++ app/computerScience/development/php/chat/message.php
@@ -0,0 +1,14 @@
+<?php
+  
+  include 'connect.db.php';
+  
+  switch( $_REQUEST['action]) {
+    case "sendMessage":
+      
+      $query = $db->prepare("INSERT INTO messages SET message=?");
+      
+      $query->execute([$_REQUEST['message']]);
+    break;
+    
+  }
+?>
