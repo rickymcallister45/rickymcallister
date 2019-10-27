a44e326bbeccc6e777f32b4f9c0b965dcb1f1e35
Ricky McAllister <rickymcallister45@gmail.com>
2019-10-27 01:16:32 -0400

    Delete soundboard.js

--- app/php/soundboard/soundboard.js
+++
@@ -1,6 +0,0 @@
-$('.sounds').click(function() {
-        var name = $(this).text();
-        var audio = new Audio('../../../files/mp3/' + name + '.mp3');
-        console.log(name);
-        audio.play();
-});
