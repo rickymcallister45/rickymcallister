3f3aff362bee107754700f4cda148cb7bdd33acb
Ricky McAllister <rickymcallister45@gmail.com>
2019-10-27 01:21:06 -0400

    Create head.php

---
+++ app/computerScience/development/php/todo/php/head.php
@@ -0,0 +1,13 @@
+<?php
+	include = "dbh.todo.php";
+	include = "todo.insert.php";
+	include = "todo.display.php";
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>rickyMcAllister.com</title>
+		<meta name="viewport" content="width=device-width. initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
