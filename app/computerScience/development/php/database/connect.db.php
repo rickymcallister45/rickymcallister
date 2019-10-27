904615af95a4fb1ab5b2fa4e5383c293a2216ecd
Ricky McAllister <rickymcallister45@gmail.com>
2019-10-27 01:11:49 -0400

    Create connect.db.php

---
+++ app/computerScience/development/php/database/connect.db.php
@@ -0,0 +1,13 @@
+<?php
+$dbServername ="localhost";
+$dbUsername = "newuser";
+$dbPassword = "password";
+$dbName = "test";
+$db = @new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
+/*
+if($db->connect_error){
+  echo $db->connect_error;
+}
+echo "Connection Successful";
+*/
+?>
