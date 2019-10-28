a60aa3b68ec2e3d6eeee19b4ea11cf59268441a6
Ricky McAllister <rickymcallister45@gmail.com>
2019-10-28 06:12:19 -0400

    Update app.js

--- app/computerScience/development/Vue/workingWithDatabase/js/app.js
+++ app/computerScience/development/Vue/workingWithDatabase/js/app.js
@@ -1,6 +1,12 @@
 var vm = new Vue({
   el: '#app',
   data: {
+    new_item: {
+	brand: '',
+	model: '',
+	engine: '1',
+	gearbox: '1',
+    },
     car_info_set: [],
     err_msg: 'Sorry, no car in record',
     url: 'include/interface.php', 
@@ -8,7 +14,41 @@ var vm = new Vue({
   created: function(){
 	this.retrieve_all();
 },
+  computed: {
+	btn_switch: function(){
+		if(this.new_item.brand.length >=1 && this.new_item.model.length >= 1){
+			return false;
+		}else{
+			return true;
+		}
+	}
+},
   methods: {
+	create_item: function(){
+		var self = this;
+		$.post({
+			url: self.url,
+			data: {
+				action: 'create_item',
+				new_item: self.new_item
+			}
+		})
+		.always(function(data){
+			self.car.info_set = [];
+			self.err_msg = '';
+		})
+		.done(function(data){
+			var result = JSON.parse(data);
+			if(result[0]){
+				self.retrieve_all();
+			}else{
+				self.err_msg = result[1];
+			}
+		})
+		.fail(function(data){
+			self.err_msg = data.statusText;
+		})
+},
 	retrieve_all: function(){
 		var self = this;
 		$.ajax({
