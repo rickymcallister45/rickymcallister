bb34eef42607b2367a1d22e7e8ad19c9ecf1cfa2
Ricky McAllister <rickymcallister45@gmail.com>
2019-10-28 06:04:01 -0400

    Update index.php

--- app/computerScience/development/Vue/workingWithDatabase/index.php
+++ app/computerScience/development/Vue/workingWithDatabase/index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-<html>
   <head>
-	<link rel='stylesheet' type='text/css' href='css/style.css'>
-	
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
 	<script type='text/javascript' src='https://cdn.jsdelivr.net/npm/vue/dist/vue.js'></script>
-  	<script type='text/javascript' src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
+  	<script type='text/javascript' src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
   </head>
+    
   <body>
-    <p>339</p>
+    <p>341</p>
     <div id='app'>
       <ul id='list' v-if='car_info_set.length'>
         <li v-for='element in car_info_set'>
@@ -18,6 +18,36 @@
         </li>
       </ul>
       <p id='err_msg' v-else>{{err_msg}}</p>
+	<hr/>
+	<ul id='create_item'>
+		<li>
+			<span>Brand: </span>
+			<input placeholder='please enter brand' v-model='new_item.brand' v-model.trim='new_item.model'/>
+		</li>
+		<li>
+			<span>Model: </span>
+			<input  placeholder='please enter model' v-model='new_item.model' v-model.trim='new_item.brand'/>
+		</li>
+		<li>
+			<span>Engine: </span>
+			<select v-model='new_item.engine'>
+				<option value='1'>Petrol</option>
+				<option value='2'>Diesel</option>
+				<option value='3'>Electric</option>
+				<option value='4'>Hybrid</option>
+				<option value='5'>Hydrogen</option>
+			</select>
+		</li>
+		<li>
+			<span>Automatic: </span>
+			<input type='radio' value='1' v-model='new_item.gearbox'/>
+			<span>Manual: </span>
+			<input type'radio' value='2' v-model='new_item.gearbox'/>
+		</li>
+		<li>
+			<button v-bind:disabled = 'btn_switch'>Create</button>
+		</li>
+	</ul>
     </div>
   <script type='text/javascript' src='js/app.js'></script>
   
