1794b54f573320d919e8c50a8a74dfcf30ba178b
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-08 05:27:33 -0500

    Update functions.php

--- app/computerScience/development/php/store/resources/functions.php
+++ app/computerScience/development/php/store/resources/functions.php
@@ -1,5 +1,27 @@
 <?php
 
-echo "from functions";
+function redirect() {
+  header("Location: $location ")
+}
 
+function query($sql) {
+  global $connection;
+    return mysqli_query($connection, $sql);
+}
+
+function confirm($result) {
+  if(!result) {
+    die("Query failed!" . mysqli_error($connection));
+  }
+}
+
+function escape_string($string) {
+  global $connection;
+    return mysqli_real_escape_string($connection, $string);
+}
+
+function fetch_array($result) {
+  
+  return myswli_fetch_array($send_query)
+}
 ?>
