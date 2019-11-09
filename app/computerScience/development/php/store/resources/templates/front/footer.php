aef039364b79929f263d3c9067b1c36725015a2a
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-09 02:58:38 -0500

    Update footer.php

--- app/computerScience/development/php/store/resources/templates/front/footer.php
+++ app/computerScience/development/php/store/resources/templates/front/footer.php
@@ -1,26 +1,23 @@
+<?php
 
-    <div class="container">
-
+echo "<div class='container'>
         <hr>
-
-        <!-- Footer -->
+        
         <footer>
-            <div class="row">
-                <div class="col-lg-12">
+            <div class='row'>
+                <div class='col-lg-12'>
                     <p>Copyright &copy; Your Website 2030</p>
                 </div>
             </div>
         </footer>
 
-    </div>
-    <!-- /.container -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+      </div>
+   
+      <script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
+      <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'></script>
 
 </body>
 
-</html>
+</html>";
+
+?>
