952ccc8e36e047422f0a939edb264910a1155303
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-09 23:27:18 -0500

    Update index.php

--- app/computerScience/development/php/store/public/index.php
+++ app/computerScience/development/php/store/public/index.php
@@ -1,13 +1,13 @@
 <?php require_once('../resources/config.php'); ?>
 
-<?php include(TEMPLATE_FRONT . DS . 'header.php'); ?>
+<?php include('../resources/header.php'); ?>
 
     <!-- Page Content -->
     <div class="container">
 
         <div class="row">
 
-           <?php include(TEMPLATE_FRONT . DS . 'side_nav.php'); ?>
+           <?php include('../resources/side_nav.php'); ?>
 
             <div class="col-md-9">
 
@@ -15,7 +15,7 @@
 
                     <div class="col-md-12">
                       
-                        <?php include(TEMPLATE_FRONT . DS . 'slider.php'); ?>
+                        <?php include('../resources/slider.php'); ?>
                         
                     </div>
 
@@ -150,4 +150,4 @@
     <!-- /.container -->
 
 
-<?php include(TEMPLATE_FRONT . DS . 'footer.php'); ?>
+<?php include('../resources/footer.php'); ?>
