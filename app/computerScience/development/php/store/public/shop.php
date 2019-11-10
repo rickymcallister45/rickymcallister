d38fd87d6885c286068b6fd974d03baa6f355158
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-10 06:27:09 -0500

    Create shop.php

---
+++ app/computerScience/development/php/store/public/shop.php
@@ -0,0 +1,20 @@
+<?php require_once('../resources/config.php'); ?>
+
+<?php include('../resources/header.php'); ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+       <h1>Shop</h1>
+
+        <hr>
+
+        <div class="row text-center">
+
+          get_products_shop_page();
+
+        </div>
+        
+        <hr>
+        
+<?php include('../resources/footer.php'); ?>
