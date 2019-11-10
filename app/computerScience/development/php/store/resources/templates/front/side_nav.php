a21a5335bbfdc5811a03abdf60190dd0459de8eb
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-10 04:07:40 -0500

    Update side_nav.php

--- app/computerScience/development/php/store/resources/templates/front/side_nav.php
+++ app/computerScience/development/php/store/resources/templates/front/side_nav.php
@@ -4,16 +4,8 @@
                   
       <?php
                   
-        $displayCategories = "SELECT * FROM categories";
-        $send_query = mysqli_query($connection, $query);
-          
-          if(!send_query) {
-            die('Query Failed' . mysqli_error($connection));
-          }
-                  
-          while($row = mysqli_fetch_assoc($send_query)) {
-            echo "<a href='#' class='list-group-item'>{$row['catregory_title']} 1</a>";
-          }
+        get_categories();
+        
       ?>
                   
     </div>
