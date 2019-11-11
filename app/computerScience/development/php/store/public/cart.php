e03f7b5a3fdc06adae3d5a862e7c12577479bf5c
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-11 06:53:16 -0500

    Update cart.php

--- app/computerScience/development/php/store/public/cart.php
+++ app/computerScience/development/php/store/public/cart.php
@@ -1,5 +1,6 @@
 <?php require_once('../resources/config.php'); ?>
-
+<?php require_once("./cart.php"); ?>
+<?php include('../resourses/templates/front/header.php'); ?>
 
 <?php
 
@@ -34,5 +35,46 @@ if(isset($_GET['remove'])) {
   $_SESSION['product_' . $_GET['delete']] == '0';
   redirect('./checkout.php');
 }
+
+
+
+function cart() {
+  $total = 0;
+  foreach($_SESSION as$name => $value) {
+    if($value > 0) {
+      if(substr($name, 0, 8) == "product_") {
+        $length = strlen($name - 8);
+        $id = substr($name, 8, $length);
+        
+      $query = query("SELECT * FROM products WHERE product_id=" . escape_string($id) . " ");
+      confirm($query);
+  
+  while($row = fetch_array($query)) {
+    $subtotle = $row['product_price'] * $value;
+    
+    $product = <<<DELIMETER
+       
+       <tr>
+         <td>{row['product_title']}</td>
+         <td>&#36;{row['product_price']}</td>
+         <td>{$value}</td>
+         <td>&#36;{$subtotal}</td>
+         <td>
+            <a class='btn btn-warning' href='./cart.php?remove={$row['product_id']}'><span class='glyphicon glyphicon-minus'></span></a>
+            <a class='btn btn-success' href='./cart.php?add={$row['product_id']}'><span class='glyphicon glyphicon-plus'></span></a>
+            <a class='btn btn-danger' href='./cart.php?delete={$row['product_id']}'><span class='glyphicon glyphicon-remove'></span></a>
+         </td>
+        </tr>
+        
+DELIMETER;
+  
+  echo $product;
+          }
+$_SESSION['item_total'] = $total += $subtotal;
+        }
+      }
+    
+    }
+  }
 
 ?>
