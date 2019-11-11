fefc1e761a867736a10f13444a6ec7d3282912e1
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-11 05:30:15 -0500

    Update cart.php

--- app/computerScience/development/php/store/public/cart.php
+++ app/computerScience/development/php/store/public/cart.php
@@ -1,1 +1,38 @@
 <?php require_once('../resources/config.php'); ?>
+
+
+<?php
+
+ if(isset($_GET['add'])) {
+   
+   $query = query("SELECT FROM products WHERE product_id=" . escape_string($_GET['add']));
+   confirm($query);
+   
+   while($row = fetch_array($query)) {
+     
+     if($row['product_quantity'] != $_SESSION['product_' . $_GET['add']]) {
+       $_SESSION['product_' . $_GET['add']] +=1;
+       redirect("./checkout.php");
+     }else{
+       set_message('We only have ' . $row['product_quantity'] . " " . "available");
+       redirect("./checkout.php");
+      }
+    }
+  }
+
+ if(isset($_GET['remove'])) {
+   
+   $_SESSION['product_' . $_GET['remove']] --;
+    if($_SESSION['product_' . $_GET['remove']] < 1) {
+      redirect('./checkout.php');
+    }else{
+      redirect(',/checkout.php');
+    }
+ }
+
+if(isset($_GET['remove'])) {
+  $_SESSION['product_' . $_GET['delete']] == '0';
+  redirect('./checkout.php');
+}
+
+?>
