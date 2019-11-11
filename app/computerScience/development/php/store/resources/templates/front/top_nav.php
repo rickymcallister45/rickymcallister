e398828a6ef7c4d979fb080b00ce2bd2408ed234
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-11 01:25:33 -0500

    Update top_nav.php

--- app/computerScience/development/php/store/resources/templates/front/top_nav.php
+++ app/computerScience/development/php/store/resources/templates/front/top_nav.php
@@ -24,7 +24,7 @@
               <a href='admin'>Admin</a>
             </li>
             <li>
-              <a href='checkout.html'>Checkout</a>
+              <a href='./checkout.php'>Checkout</a>
             </li>
             <li>
               <a href='./contact.php'>Contact</a>
