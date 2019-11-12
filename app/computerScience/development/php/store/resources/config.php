ae4acc57f3252ef6178306ad20b83204a2aacf34
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-11 20:37:39 -0500

    Update config.php

--- app/computerScience/development/php/store/resources/config.php
+++ app/computerScience/development/php/store/resources/config.php
@@ -5,17 +5,17 @@ ob_start();
 session_start();
 //session_destroy();
 
-defined("DS") ? null : define("DS", DIRECTORY_SEPERATOR);
+//defined("DS") ? null : define("DS", DIRECTORY_SEPERATOR);
 
 
-defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "templates/front");
+//defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "templates/front");
 
-defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
+//defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
 
 
 defined("DB_HOST") ? null : define("DB_HOST", "localhost");
-defined("DB_USER") ? null : define("DB_USER", "root");
-defined("DB_PASS") ? null : define("DB_PASS", "newuser");
+defined("DB_USER") ? null : define("DB_USER", "newuser");
+defined("DB_PASS") ? null : define("DB_PASS", "password");
 defined("DB_NAME") ? null : define("DB_NAME", "ecom_db_test");
 
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
