c0814666e9cd51f2498142ab9cb8d359ffe1c9be
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-15 09:46:50 -0500

    Update top_nav.php

--- app/computerScience/development/php/store/resources/templates/front/top_nav.php
+++ app/computerScience/development/php/store/resources/templates/front/top_nav.php
@@ -1,6 +1,6 @@
  <?php
  
- echo "<div class='container'>
+echo "<div class='container'>
  
         <div class='navbar-header'>
           <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>
