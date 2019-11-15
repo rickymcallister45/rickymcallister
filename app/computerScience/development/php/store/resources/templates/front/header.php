dbe049e81936061669b99d02f03482d8c7faf52d
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-15 09:29:13 -0500

    Update header.php

--- app/computerScience/development/php/store/resources/templates/front/header.php
+++ app/computerScience/development/php/store/resources/templates/front/header.php
@@ -1,26 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+
+echo "<!DOCTYPE html>
+<html lang='en'>
 
 <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta name='description' content=''>
+    <meta name='author' content=''>
 
     <title>rickyMcAllister</title>
     
     <link rel='icon' href='./favicon.ico' type='image/x-icon'>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/shop-homepage.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+    <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' rel='stylesheet'>
+    <link href='css/shop-homepage.css' rel='stylesheet'>
+    <link href='css/style.css' rel='stylesheet'>
 
 </head>
 
 <body>
 
     <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <nav class='navbar navbar-inverse navbar-fixed-top' role='navigation'>
        <?php include('../resourses/templates/front/top_nav.php'); ?>
-    </nav>
+    </nav>"
+    
+    ?>
