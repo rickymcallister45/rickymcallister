5257f3df29c0dc1c3df2e78307e7688aa5a037e2
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-16 01:28:29 -0500

    Update config.php

--- resources/store/resources/config.php
+++ resources/store/resources/config.php
@@ -9,12 +9,12 @@ session_start();
 defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
 
 
-defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "resources/store/templates/front");
+defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "resources/store/resources/templates/front");
 
-defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "resources/store/templates/back");
+defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "resources/store/resources/templates/back");
 
 
-defined("UPLOAD_DIRECTORY") ? null : define("UPLOAD_DIRECTORY", __DIR__ . DS . "resources/store/uploads");
+defined("UPLOAD_DIRECTORY") ? null : define("UPLOAD_DIRECTORY", __DIR__ . DS . "resources/store/resources/uploads");
 
 
 defined("DB_HOST") ? null : define("DB_HOST", "localhost");
