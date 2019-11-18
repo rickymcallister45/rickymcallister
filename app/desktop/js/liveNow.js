3c29a12a67de33c5e1622d3d1992de88e51d816e
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-18 02:51:44 -0500

    Update liveNow.js

--- app/desktop/js/liveNow.js
+++ app/desktop/js/liveNow.js
@@ -1,17 +1,23 @@
+var liveNowJsonData;
+
 function liveNow(){
 var request = new XMLHttpRequest();
 var url = 'https://www.googleapis.com/youtube/v3/search?';
 var snippet = 'part=snippet&channelId=UCHUrEa6E1l5vwzvXMGEPScg&type=video&eventType=live&key=AIzaSyATpzdfvkMW6jt9rRpj99Dp57LwW77nxMA';
 
-var jsonData;
-    request.open('GET', url + snippet + '.json', true);
+
+    request.open('GET', url + snippet , true);
     request.onload = function() {
           var data = JSON.parse(request.responseText);
-          jsonData = data.items.snippet.liveBroadcastContent;
-          console.log(jsonData);
-          
+          if(data.pageInfo.totalResults === 0){
+            liveNowJsonData = 'offline';
+            console.log(liveNowJsonData);
+          }else{
+            liveNowJsonData = data.items.snippet.liveBroadcastContent;
+            console.log(liveNowJsonData);
+          }
       }
-      request.send();
+    request.send();
 }
 
 liveNow();
