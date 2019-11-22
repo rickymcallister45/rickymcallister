16d78ccdded1d7b72274dfc4557c6d8448eed1b6
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-22 06:04:34 -0500

    Create topNav.php

---
+++ app/store/topNav.php
@@ -0,0 +1,35 @@
+<?php      
+
+echo "<nav class='navbar navbar-inverse navbar-fixed-top' role='navigation'>
+        <div class='container'>
+          <div class='navbar-header'>
+            <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>
+              <span class='sr-only'>Toggle navigation</span>
+              <span class='icon-bar'></span>
+              <span class='icon-bar'></span>
+              <span class='icon-bar'></span>
+            </button>
+              <a class='navbar-brand' href='index.html'>Start Bootstrap</a>
+          </div>
+          <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
+            <ul class='nav navbar-nav'>
+              <li>
+                <a href='#'>Shop</a>
+              </li>
+              <li>
+                <a href='#'>Login</a>
+              </li>
+              <li>
+                <a href='admin'>Admin</a>
+              </li>
+              <li>
+                <a href='checkout.html'>Checkout</a>
+              </li>
+              <li>
+                <a href='contact.html'>Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>";
+?>
