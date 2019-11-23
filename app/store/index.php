0191b4fd6a7c5be0b0e1c8d0c51149a52b5ddee4
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-23 01:22:20 -0500

    Update index.php

--- app/store/index.php
+++ app/store/index.php
@@ -1,5 +1,20 @@
 <?php
 include_once 'frontEnd/header.php';
 
+echo "<body>
+        <!-- Page Content -->
+          <div class='container'>
+            <div class='row'>
+              <!-- Side Nav -->";
+                include_once 'frontEnd/sideNav.php';
+                  echo "<div class='col-md-9'>
+              <!-- Slider -->";
+                include_once 'frontEnd/slider.php';
+                  echo "<!-- Products -->";
+                include_once 'frontEnd/sideNav.php';
+                  echo "</div>
+          </div>
+    </div>
+</body>";
 include_once 'frontEnd/footer.php';
 ?>
