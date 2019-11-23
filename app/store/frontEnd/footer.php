74bbfaa63c3eb02c86d92780f888a38d6ee1b800
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-23 00:25:13 -0500

    Create footer.php

---
+++ app/store/frontEnd/footer.php
@@ -0,0 +1,18 @@
+<?php
+echo "<h1>FOOTER WORKING</h1>
+    
+      <div class='container'>
+        <hr>
+        <footer>
+          <div class='row'>
+            <div class='col-lg-12'>
+              <p>Copyright &copy; rickyMcAllister 2019/p>
+            </div>
+           </div>
+        </footer>
+     </div>
+     <script src='https://code.jquery.com/jquery-3.4.1.js'></script>
+     <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'></script>
+</body>
+</html>";
+?>
