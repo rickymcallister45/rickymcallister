f4b0073a326f912180f5544235efead7f1c30137
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-23 02:28:44 -0500

    Update header.php

--- app/store/frontEnd/header.php
+++ app/store/frontEnd/header.php
@@ -5,8 +5,8 @@ echo "<!DOCTYPE html>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <meta name='description' content='>
-    <meta name='author' content='>
+    <meta name='description' content='rickyMcAllisterStore'>
+    <meta name='author' content='rickyMcAllister'>
     <title>rickyMcAllister</title>
     <link rel='icon' href='../../../resources/favicon/favicon.ico' type='image/x-icon'>
     
