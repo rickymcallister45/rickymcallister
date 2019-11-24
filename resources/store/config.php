da6f6dc94a5737aabb722581460980232e3b3bb9
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-24 07:55:40 -0500

    Update config.php

--- resources/store/config.php
+++ resources/store/config.php
@@ -12,11 +12,6 @@ defined("DB_NAME") ? null : define("DB_NAME", "ecom_db_test");
 
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
-if($connection){
- echo "<script>alert('database connected');</script>";
-}else{
-  echo "<script>alert('database NOT connected');</script>";
-}
 
 //require_once("../resourses/functions.php");
 
