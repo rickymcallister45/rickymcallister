e2e29c37f7e49b8a06f0bf64c9ad6fd2c439d1cd
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-24 08:02:28 -0500

    Update footer.php

--- app/store/frontEnd/footer.php
+++ app/store/frontEnd/footer.php
@@ -9,10 +9,10 @@ echo "<div class='container'>
            </div>
         </footer>
      </div>
-     //<script src='https://code.jquery.com/jquery-3.4.1.js'></script>
-     <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'></script>
+     <script src='./js/jquery.js'></script>
+     <script src='./js/bootstrap.min.js'></script>
      
-     //<script src='./js/jquery.js'></script>
+     
 </body>
 </html>";
 ?>
