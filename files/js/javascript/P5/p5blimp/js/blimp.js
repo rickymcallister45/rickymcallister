7401150cf9c52656793fa8675179012ed367c78a
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-25 05:24:56 -0500

    Update blimp.js

--- files/js/javascript/P5/p5blimp/js/blimp.js
+++ files/js/javascript/P5/p5blimp/js/blimp.js
@@ -2,13 +2,9 @@ let blimp;
 var x;
 var canvas;
 
-
-
 function preload(){
-	blimp = loadImage("../img/blimp.png");
-	
+	blimp = loadImage("blimp.png");
 }
-
 
 function setup() {
 	canvas = createCanvas(windowWidth, windowHeight);
@@ -17,27 +13,13 @@ function setup() {
 	canvas.style('z-index', '-1');
 	x = windowWidth;
 	y = 150;
-	
-
-	
-	
-
 }
-
 
 function draw() {
-
 	background(0, 61, 153);
-
 	image(blimp, x, y, 400, 300);
 	x = x + .25 * -1;
-	
 	if(x + 500 < 0 ) {
-		x = windowWidth + 500;
+	x = windowWidth + 500;
 	}
-
-	
-
 }
-
-
