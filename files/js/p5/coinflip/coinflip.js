f20e5786f0ca9a8a0eacd141e0236fef69961feb
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-25 02:32:26 -0500

    Create coinflip.js

---
+++ files/js/p5/coinflip/coinflip.js
@@ -0,0 +1,43 @@
+//* coin flip
+
+var p5;
+
+var coinFlip = function(cf){
+
+var time = 2000;
+var heads = "img/front.png";
+var tails = "img/back.png";
+var number;
+var answer;
+
+cf.decision = function(number) {
+			if (number == 1) {
+				console.log("heads");
+				answer = cf.createImg(heads);
+				} else {
+				console.log("tails");
+				answer = cf.createImg(tails);
+			}	}	
+
+cf.keyTyped =	function(){
+	if (cf.keyCode === 32) { 
+		
+		console.log("coinFlip Starting!!");
+		var number =  Math.floor((Math.random() * 2) + 1)
+		console.log(number);
+ 
+		setInterval(cf.timeIt, time);
+		cf.decision(number);
+		answer.position(825,225);
+		}else{
+		return(false);	
+
+}	}
+
+cf.timeIt = function() {
+		answer.remove();
+	}
+cf.draw = function(){}
+}
+
+var myp5 = new p5(coinFlip);
