54d523098bf662a4d839038c06fe75883a3788ca
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-25 02:27:23 -0500

    Delete getJS.js

--- files/js/javascript/js/getJS.js
+++
@@ -1,6 +0,0 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', https://api.intrinio.com/data_point?identifier={symbol}&item={tag});
-ourRequest.onload = function(){
-	console.log(ourRequest.reponseText);
-};
-ourRequest.send();
