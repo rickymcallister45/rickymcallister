74c1fa31c3eab85c6bbe04392ed87ad6887a99e4
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-26 01:31:53 -0500

    Update functions.php

--- resources/store/functions.php
+++ resources/store/functions.php
@@ -34,11 +34,12 @@ function get_products() {
   while($row = fetch_array($query)) {
     echo "<div class='col-sm-4 col-lg-4 col-md-4'>
             <div class='thumbnail'>
-              <img src='http://placehold.it/320x150' alt='>
+              <a href='./item.php?id={$row['product_id']}'><img src='{$row['product_image']}' alt='{$row['product_title']}'></a>
               <div class='caption'>
-                <h4 class='pull-right'>$24.99</h4>
-                <h4><a href='product.html'>First Product</a></h4>
-                <p>See more snippets like this online store item at <a target='_blank' href='#'>Bootsnipp - http://bootsnipp.com</a>.</p>
+                <h4 class='pull-right'>&#36;{$row['product_price']}</h4>
+                <h4><a href='#'>{$row['product_title']}</a></h4>
+                <p>{$row['product_short_description']}</p>
+                <a class='btn btn-primary' target='_blank' href='#'>Add to cart</a>
               </div>
             </div>  
           </div>";
