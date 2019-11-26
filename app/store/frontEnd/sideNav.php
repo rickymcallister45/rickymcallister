c640f29bd2b82e49b9e0f72f96d76966db1153d1
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-26 01:57:16 -0500

    Update sideNav.php

--- app/store/frontEnd/sideNav.php
+++ app/store/frontEnd/sideNav.php
@@ -20,6 +20,6 @@ echo  "<a href='category.html' class='list-group-item'>Category 1</a>
        <a href='#' class='list-group-item'>Category 2</a>
        <a href='#' class='list-group-item'>Category 3</a>";
 */
-echo   "</div>
+echo "</div>
   </div>";
 ?>
