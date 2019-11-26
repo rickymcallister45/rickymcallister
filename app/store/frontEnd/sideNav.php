9b58ad37abad75b0eaa14174fa819608c7c0f1a0
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-26 01:56:59 -0500

    Update sideNav.php

--- app/store/frontEnd/sideNav.php
+++ app/store/frontEnd/sideNav.php
@@ -3,7 +3,8 @@
 echo "<div class='col-md-3'>
         <p class='lead'>Ricky McAllister's Store</p>
           <div class='list-group'>";
-        
+                get_categories();
+/*
         $query = "SELECT * FROM categories";
         $send_query = mysqli_query($connection, $query);
         
@@ -14,7 +15,7 @@ echo "<div class='col-md-3'>
         while($row = myswli_fetch_assoc($send_query)) {
              echo "<a href='#' class='list-group-item'>{$row['category_title']}</a>";
         }
-/*
+
 echo  "<a href='category.html' class='list-group-item'>Category 1</a>
        <a href='#' class='list-group-item'>Category 2</a>
        <a href='#' class='list-group-item'>Category 3</a>";
