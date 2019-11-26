549ca5ac16304831674c26459e63cf2f795e8f6a
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-26 06:40:35 -0500

    Update category.php

--- app/store/category.php
+++ app/store/category.php
@@ -4,91 +4,63 @@ include_once 'frontEnd/header.php';
 echo "<body>";
         include_once 'frontEnd/topNav.php';
 echo "<!-- Header -->
-  <header class='bg-primary py-5 mb-5'>
-    <div class='container h-100'>
-      <div class='row h-100 align-items-center'>
-        <div class='col-lg-12'>
-          <h1 class='display-4 text-white mt-5 mb-2'>Business Name or Tagline</h1>
-          <p class='lead mb-5 text-white-50'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non possimus ab labore provident mollitia. Id assumenda voluptate earum corporis facere quibusdam quisquam iste ipsa cumque unde nisi, totam quas ipsam.</p>
-        </div>
-      </div>
-    </div>
-  </header>
+        <header class='bg-primary py-5 mb-5'>
+          <div class='container h-100'>
+            <div class='row h-100 align-items-center'>
+              <div class='col-lg-12'>
+                <h1 class='display-4 text-white mt-5 mb-2'>Business Name or Tagline</h1>
+                <p class='lead mb-5 text-white-50'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non possimus ab labore provident mollitia. Id assumenda voluptate earum corporis facere quibusdam quisquam iste ipsa cumque unde nisi, totam quas ipsam.</p>
+              </div>
+            </div>
+          </div>
+        </header>
+<!-- Page Content -->
+       <div class='container'>
 
-  <!-- Page Content -->
-  <div class='container'>
+        <!-- Jumbotron Header -->
+        <header class='jumbotron hero-spacer'>
+            <h1>A Warm Welcome!</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
+            <p><a class='btn btn-primary btn-large'>Call to action!</a>
+            </p>
+        </header>
 
-    <div class='row'>
-      <div class='col-md-8 mb-5'>
-        <h2>What We Do</h2>
         <hr>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A deserunt neque tempore recusandae animi soluta quasi? Asperiores rem dolore eaque vel, porro, soluta unde debitis aliquam laboriosam. Repellat explicabo, maiores!</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis optio neque consectetur consequatur magni in nisi, natus beatae quidem quam odit commodi ducimus totam eum, alias, adipisci nesciunt voluptate. Voluptatum.</p>
-        <a class='btn btn-primary btn-lg' href='#'>Call to Action &raquo;</a>
-      </div>
-      <div class='col-md-4 mb-5'>
-        <h2>Contact Us</h2>
-        <hr>
-        <address>
-          <strong>Start Bootstrap</strong>
-          <br>3481 Melrose Place
-          <br>Beverly Hills, CA 90210
-          <br>
-        </address>
-        <address>
-          <abbr title='Phone'>P:</abbr>
-          (123) 456-7890
-          <br>
-          <abbr title='Email'>E:</abbr>
-          <a href='mailto:#'>name@example.com</a>
-        </address>
-      </div>
-    </div>
-    <!-- /.row -->
 
-    <div class='row'>
-      <div class='col-md-4 mb-5'>
-        <div class='card h-100'>
-          <img class='card-img-top' src='http://placehold.it/300x200' alt=''>
-          <div class='card-body'>
-            <h4 class='card-title'>Card title</h4>
-            <p class='card-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
-          </div>
-          <div class='card-footer'>
-            <a href='#' class='btn btn-primary'>Find Out More!</a>
-          </div>
+        <!-- Title -->
+        <div class='row'>
+            <div class='col-lg-12'>
+                <h3>Latest Features</h3>
+            </div>
         </div>
-      </div>
-      <div class='col-md-4 mb-5'>
-        <div class='card h-100'>
-          <img class='card-img-top' src='http://placehold.it/300x200' alt=''>
-          <div class='card-body'>
-            <h4 class='card-title'>Card title</h4>
-            <p class='card-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus totam ut praesentium aut.</p>
-          </div>
-          <div class='card-footer'>
-            <a href='#' class='btn btn-primary'>Find Out More!</a>
-          </div>
-        </div>
-      </div>
-      <div class='col-md-4 mb-5'>
-        <div class='card h-100'>
-          <img class='card-img-top' src='http://placehold.it/300x200' alt=''>
-          <div class='card-body'>
-            <h4 class='card-title'>Card title</h4>
-            <p class='card-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-          </div>
-          <div class='card-footer'>
-            <a href='#' class='btn btn-primary'>Find Out More!</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- /.row -->
+        <!-- /.row -->
 
-  </div>
-  <!-- /.container -->";
+        <!-- Page Features -->";
+        
+        get_products_on_category_page();
+        
+        
+        /*
+     echo "<div class='row text-center'>
 
+            <div class='col-md-3 col-sm-6 hero-feature'>
+                <div class='thumbnail'>
+                    <img src='http://placehold.it/800x500' alt=''>
+                    <div class='caption'>
+                        <h3>Feature Label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <p>
+                            <a href='#' class='btn btn-primary'>Buy Now!</a> <a href='#' class='btn btn-default'>More Info</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+          </div>";
+          */
+          
+          
+          
+   echo "</div>";
 include_once 'frontEnd/footer.php';
 echo "</body>";
 ?>
