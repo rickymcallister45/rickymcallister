b73a6b9592de976a210955cb66694d3fc3fab65d
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-26 01:36:45 -0500

    Update products.php

--- app/store/frontEnd/products.php
+++ app/store/frontEnd/products.php
@@ -1,8 +1,9 @@
 <?php
 
-echo "<div class='row'>
-
-                    <div class='col-sm-4 col-lg-4 col-md-4'>
+echo "<div class='row'>";
+              get_products();
+/*
+              echo "<div class='col-sm-4 col-lg-4 col-md-4'>
                         <div class='thumbnail'>
                             <img src='http://placehold.it/320x150' alt='>
                             <div class='caption'>
@@ -111,8 +112,8 @@ echo "<div class='row'>
                             
                         </div>
                     </div>
-
-                    <div class='col-sm-4 col-lg-4 col-md-4'>
+*/
+               echo "<div class='col-sm-4 col-lg-4 col-md-4'>
                         <h4><a href='#'>Like this template?</a>
                         </h4>
                         <p>If you like this template, then check out <a target='_blank' href='http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/'>this tutorial</a> on how to build a working review system for your online store!</p>
