0a55af726462b303dd259171d48758dd2842d6cd
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-26 00:33:06 -0500

    Update functions.php

--- resources/store/functions.php
+++ resources/store/functions.php
@@ -28,6 +28,24 @@ function fetch_array($result) {
 
 // FRONTEND FUNCTIONS
 
+function get_products() {
+  $query = query('SELECT * FROM products');
+  confirm($query);
+  while($row = fetch_array($query)) {
+    echo "<div class='col-sm-4 col-lg-4 col-md-4'>
+            <div class='thumbnail'>
+              <img src='http://placehold.it/320x150' alt='>
+              <div class='caption'>
+                <h4 class='pull-right'>$24.99</h4>
+                <h4><a href='product.html'>First Product</a></h4>
+                <p>See more snippets like this online store item at <a target='_blank' href='#'>Bootsnipp - http://bootsnipp.com</a>.</p>
+              </div>
+            </div>  
+          </div>";
+  }
+}
+
+
 // BACKEND FUNCTIONS
 
 ?>
