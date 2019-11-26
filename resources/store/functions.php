b795315fd13a8dc5ab0a247d8866a434d4ca47b3
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-25 23:27:05 -0500

    Update functions.php

--- resources/store/functions.php
+++ resources/store/functions.php
@@ -1,13 +1,33 @@
 <?php
 // HELPER FUNCTIONS
 
-function redirect()
+function redirect($location) {
+  header('LocationL $location');
+}
 
+function query($sql) {
+  global $connection;
+  return mysqli_query($conncection, $sql);
+}
 
+function confirm($result) {
+  global $connection;
+  if(!$result) {
+    die('Query Failed ' . mysqli_error($connection));
+  }
+}
+
+function escape_string($string) {
+  global $connection;
+  return mysqli_real_escape_string($connection, $string);
+}
+
+function fetch_array($result) {
+  return mysqli_fetch_array($result);
+}
 
 // FRONTEND FUNCTIONS
 
 // BACKEND FUNCTIONS
-
 
 ?>
