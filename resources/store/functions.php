1de520c3d480540838f3afa5d341155527ec9116
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-26 05:53:33 -0500

    Update functions.php

--- resources/store/functions.php
+++ resources/store/functions.php
@@ -54,6 +54,24 @@ function get_categories() {
   }
 }
 
+function get_products_on_category_page() {
+  $query = query("SELECT * FROM products WHERE product_category_id = " . escape_string($_GET['id']) . "");
+  confirm($query);
+  while($row = fetch_array($query)) {
+    echo "<div class='col-sm-4 col-lg-4 col-md-4'>
+            <div class='thumbnail'>
+              <a href='./item.php?id={$row['product_id']}'><img src='{$row['product_image']}' alt='{$row['product_title']}'></a>
+              <div class='caption'>
+                <h4 class='pull-right'>&#36;{$row['product_price']}</h4>
+                <h4><a href='./item.php?id={$row['product_id']}'>{$row['product_title']}</a></h4>
+                <p>{$row['product_short_description']}</p>
+                <a class='btn btn-primary' target='_blank' href='#'>Add to cart</a>
+              </div>
+            </div>  
+          </div>";
+  }
+}
+
 
 // BACKEND FUNCTIONS
 
