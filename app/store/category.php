339a45bcf9e1e2c85c8ae3830e96c406df64be8d
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-26 02:29:38 -0500

    Update category.php

--- app/store/category.php
+++ app/store/category.php
@@ -1,54 +1,9 @@
-<!DOCTYPE html>
-<html lang='en'>
-
-<head>
-
-  <meta charset='utf-8'>
-  <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
-  <meta name='description' content=''>
-  <meta name='author' content=''>
-
-  <title>Business Frontpage - Start Bootstrap Template</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href='./css/bootstrap.min.css' rel='stylesheet'>
-
-  <!-- Custom styles for this template -->
-  <link href='./css/business-frontpage.css' rel='stylesheet'>
-
-</head>
-
-<body>
-
-  <!-- Navigation -->
-  <nav class='navbar navbar-expand-lg navbar-dark bg-dark fixed-top'>
-    <div class='container'>
-      <a class='navbar-brand' href='#'>Start Bootstrap</a>
-      <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarResponsive' aria-controls='navbarResponsive' aria-expanded='false' aria-label='Toggle navigation'>
-        <span class='navbar-toggler-icon'></span>
-      </button>
-      <div class='collapse navbar-collapse' id='navbarResponsive'>
-        <ul class='navbar-nav ml-auto'>
-          <li class='nav-item active'>
-            <a class='nav-link' href='#'>Home
-              <span class='sr-only'>(current)</span>
-            </a>
-          </li>
-          <li class='nav-item'>
-            <a class='nav-link' href='#'>About</a>
-          </li>
-          <li class='nav-item'>
-            <a class='nav-link' href='#'>Services</a>
-          </li>
-          <li class='nav-item'>
-            <a class='nav-link' href='#'>Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
-  <!-- Header -->
+<?php
+include_once '../../resources/store/test.php';
+include_once 'frontEnd/header.php';
+echo "<body>";
+        include_once 'frontEnd/topNav.php';
+echo "<!-- Header -->
   <header class='bg-primary py-5 mb-5'>
     <div class='container h-100'>
       <div class='row h-100 align-items-center'>
@@ -132,20 +87,8 @@
     <!-- /.row -->
 
   </div>
-  <!-- /.container -->
+  <!-- /.container -->";
 
-  <!-- Footer -->
-  <footer class='py-5 bg-dark'>
-    <div class='container'>
-      <p class='m-0 text-center text-white'>Copyright &copy; Your Website 2019</p>
-    </div>
-    <!-- /.container -->
-  </footer>
-
-  <!-- Bootstrap core JavaScript -->
-  <script src='./js/jquery.min.js'></script>
-  <script src='./js/bootstrap.bundle.min.js'></script>
-
-</body>
-
-</html>
+include_once 'frontEnd/footer.php';
+echo "</body>";
+?>
