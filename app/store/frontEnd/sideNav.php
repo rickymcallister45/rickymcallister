599dd9ed9be42c015bec5e5a880fb50fe27fc1e4
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-25 22:21:02 -0500

    Update sideNav.php

--- app/store/frontEnd/sideNav.php
+++ app/store/frontEnd/sideNav.php
@@ -2,10 +2,23 @@
 
 echo "<div class='col-md-3'>
         <p class='lead'>Shop Name</p>
-          <div class='list-group'>
-            <a href='category.html' class='list-group-item'>Category 1</a>
-            <a href='#' class='list-group-item'>Category 2</a>
-            <a href='#' class='list-group-item'>Category 3</a>
-          </div>
-       </div>";
+          <div class='list-group'>";
+        
+        $query = "SELECT * FROM categories";
+        $send_query = mysqli_query($connection, $query);
+        
+        if(!$send_query) {
+                die('QUERY FAILED' . mysqli_error($connection));
+        }
+        
+        while($row = myswli_fetch_assoc($send_query)) {
+             echo "<a href='#' class='list-group-item'>{$row['category_title']}</a>";
+        }
+/*
+echo  "<a href='category.html' class='list-group-item'>Category 1</a>
+       <a href='#' class='list-group-item'>Category 2</a>
+       <a href='#' class='list-group-item'>Category 3</a>";
+*/
+echo   "</div>
+  </div>";
 ?>
