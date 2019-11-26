f196a894a868f68932f4a3c964935f0a7b8a7852
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-26 04:03:53 -0500

    Update item.php

--- app/store/item.php
+++ app/store/item.php
@@ -16,130 +16,124 @@ $query = query("SELECT * FROM products WHERE product_id =" . escape_string($_GET
         echo "<div class='col-md-9'>
                 <div class='row'>
                   <div class='col-md-7'>
-                    <img class='img-responsive' src='http://placehold.it/700x600' alt=''>
+                    <img class='img-responsive' src='{$row[product_image_large]}' alt='{$row[product_title]}'>
                   </div>
                   <div class='col-md-5'>
-                  <div class='thumbnail'>
-                  <div class='caption-full'>
-                  <h4><a href='#'>Javascript Course</a> </h4>
-                  <hr>
-                  <h4 class=''>$24.99</h4>
-                  <div class='ratings'>
-                  <p>
-                    <span class='glyphicon glyphicon-star'></span>
-                    <span class='glyphicon glyphicon-star'></span>
-                    <span class='glyphicon glyphicon-star'></span>
-                    <span class='glyphicon glyphicon-star'></span>
-                    <span class='glyphicon glyphicon-star-empty'></span>
-                     4.0 stars
-                  </p>
-                </div>
+                    <div class='thumbnail'>
+                      <div class='caption-full'>
+                        <h4>{$row[product_title]}</h4>
+                        <hr>
+                        <h4 class=''>&#36;{$row[product_price]}</h4>
+                        <div class='ratings'>
+                          <p>
+                            <span class='glyphicon glyphicon-star'></span>
+                            <span class='glyphicon glyphicon-star'></span>
+                            <span class='glyphicon glyphicon-star'></span>
+                            <span class='glyphicon glyphicon-star'></span>
+                            <span class='glyphicon glyphicon-star-empty'></span>
+                            4.0 stars
+                          </p>
+                        </div>
           
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        <p>{$row[product_short_description]}</p>
 
-                <form action=''>
-                  <div class='form-group'>
-                    <input type='submit' class='btn btn-primary' value='ADD TO CART'>
+                        <form action=''>
+                          <div class='form-group'>
+                            <input type='submit' class='btn btn-primary' value='ADD TO CART'>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
                   </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-<hr>
-
-  <div class='row'>
-    <div role='tabpanel'>
-      <!-- Nav tabs -->
-        <ul class='nav nav-tabs' role='tablist'>
-          <li role='presentation' class='active'><a href='#home' aria-controls='home' role='tab' data-toggle='tab'>Description</a></li>
-          <li role='presentation'><a href='#profile' aria-controls='profile' role='tab' data-toggle='tab'>Reviews</a></li>
-        </ul>
-      <!-- Tab panes -->
-      <div class='tab-content'>
-        <div role='tabpanel' class='tab-pane active' id='home'>
-          <p></p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-        </div>
-        <div role='tabpanel' class='tab-pane' id='profile'>
-          <div class='col-md-6'>
-            <h3>2 Reviews From </h3>
-            <hr>
-              <div class='row'>
-                <div class='col-md-12'>
-                  <span class='glyphicon glyphicon-star'></span>
-                  <span class='glyphicon glyphicon-star'></span>
-                  <span class='glyphicon glyphicon-star'></span>
-                  <span class='glyphicon glyphicon-star'></span>
-                  <span class='glyphicon glyphicon-star-empty'></span>
-                    Anonymous
-                  <span class='pull-right'>10 days ago</span>
-                  <p>This product was great in terms of quality. I would definitely buy another!</p>
                 </div>
-              </div>
               <hr>
-              <div class='row'>
-                <div class='col-md-12'>
-                  <span class='glyphicon glyphicon-star'></span>
-                  <span class='glyphicon glyphicon-star'></span>
-                  <span class='glyphicon glyphicon-star'></span>
-                  <span class='glyphicon glyphicon-star'></span>
-                  <span class='glyphicon glyphicon-star-empty'></span>
-                  Anonymous
-                  <span class='pull-right'>12 days ago</span>
-                  <p>I've alredy ordered another one!</p>
+                <div class='row'>
+                  <div role='tabpanel'>
+                    <ul class='nav nav-tabs' role='tablist'>
+                      <li role='presentation' class='active'><a href='#home' aria-controls='home' role='tab' data-toggle='tab'>Description</a></li>
+                      <li role='presentation'><a href='#profile' aria-controls='profile' role='tab' data-toggle='tab'>Reviews</a></li>
+                    </ul>
+                    <div class='tab-content'>
+                      <div role='tabpanel' class='tab-pane active' id='home'>
+                        <p>{$row[product_description]}</p>
+                      </div>
+                      <div role='tabpanel' class='tab-pane' id='profile'>
+                        <div class='col-md-6'>
+                          <h3>2 Reviews From </h3>
+                          <hr>
+                          <div class='row'>
+                            <div class='col-md-12'>
+                              <span class='glyphicon glyphicon-star'></span>
+                              <span class='glyphicon glyphicon-star'></span>
+                              <span class='glyphicon glyphicon-star'></span>
+                              <span class='glyphicon glyphicon-star'></span>
+                              <span class='glyphicon glyphicon-star-empty'></span>
+                                Anonymous
+                              <span class='pull-right'>10 days ago</span>
+                              <p>This product was great in terms of quality. I would definitely buy another!</p>
+                            </div>
+                          </div>
+                          <hr>
+                          <div class='row'>
+                            <div class='col-md-12'>
+                              <span class='glyphicon glyphicon-star'></span>
+                              <span class='glyphicon glyphicon-star'></span>
+                              <span class='glyphicon glyphicon-star'></span>
+                              <span class='glyphicon glyphicon-star'></span>
+                              <span class='glyphicon glyphicon-star-empty'></span>
+                              Anonymous
+                              <span class='pull-right'>12 days ago</span>
+                              <p>I've alredy ordered another one!</p>
+                            </div>
+                          </div>
+                          <hr>
+                          <div class='row'>
+                            <div class='col-md-12'>
+                              <span class='glyphicon glyphicon-star'></span>
+                              <span class='glyphicon glyphicon-star'></span>
+                              <span class='glyphicon glyphicon-star'></span>
+                              <span class='glyphicon glyphicon-star'></span>
+                              <span class='glyphicon glyphicon-star-empty'></span>
+                              Anonymous
+                              <span class='pull-right'>15 days ago</span>
+                              <p>I've seen some better than this, but not at this price. I definitely recommend this item.</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div class='col-md-6'>
+                          <h3>Add A review</h3>
+                          <form action='' class='form-inline'>
+                            <div class='form-group'>
+                              <label for=''>Name</label>
+                                <input type='text' class='form-control' >
+                            </div>
+                            <div class='form-group'>
+                              <label for=''>Email</label>
+                                <input type='test' class='form-control'>
+                            </div>
+                            <div>
+                              <h3>Your Rating</h3>
+                              <span class='glyphicon glyphicon-star'></span>
+                              <span class='glyphicon glyphicon-star'></span>
+                              <span class='glyphicon glyphicon-star'></span>
+                              <span class='glyphicon glyphicon-star'></span>
+                            </div>
+                            <br>
+                            <div class='form-group'>
+                              <textarea name=' id=' cols='60' rows='10' class='form-control'></textarea>
+                            </div>
+                            <br>
+                            <br>
+                            <div class='form-group'>
+                              <input type='submit' class='btn btn-primary' value='SUBMIT'>
+                            </div>
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <hr>
-              <div class='row'>
-                <div class='col-md-12'>
-                  <span class='glyphicon glyphicon-star'></span>
-                  <span class='glyphicon glyphicon-star'></span>
-                  <span class='glyphicon glyphicon-star'></span>
-                  <span class='glyphicon glyphicon-star'></span>
-                  <span class='glyphicon glyphicon-star-empty'></span>
-                  Anonymous
-                  <span class='pull-right'>15 days ago</span>
-                  <p>I've seen some better than this, but not at this price. I definitely recommend this item.</p>
-                </div>
-              </div>
-          </div>
-          <div class='col-md-6'>
-            <h3>Add A review</h3>
-            <form action='' class='form-inline'>
-              <div class='form-group'>
-                <label for=''>Name</label>
-                  <input type='text' class='form-control' >
-              </div>
-              <div class='form-group'>
-                <label for=''>Email</label>
-                  <input type='test' class='form-control'>
-              </div>
-              <div>
-                <h3>Your Rating</h3>
-                <span class='glyphicon glyphicon-star'></span>
-                <span class='glyphicon glyphicon-star'></span>
-                <span class='glyphicon glyphicon-star'></span>
-                <span class='glyphicon glyphicon-star'></span>
-              </div>
-              <br>
-              <div class='form-group'>
-                <textarea name=' id=' cols='60' rows='10' class='form-control'></textarea>
-              </div>
-              <br>
-              <br>
-              <div class='form-group'>
-                <input type='submit' class='btn btn-primary' value='SUBMIT'>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>";
+              </div>";
   }
 
 include_once 'frontEnd/footer.php';
