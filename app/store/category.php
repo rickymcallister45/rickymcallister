496d6372f6bbe0af61dbcf46c64303640b82a1c9
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-26 02:08:10 -0500

    Update category.php

--- app/store/category.php
+++ app/store/category.php
@@ -1,163 +1,150 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang='en'>
 
 <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+  <meta charset='utf-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+  <meta name='description' content=''>
+  <meta name='author' content=''>
 
-    <title>Heroic Features - Start Bootstrap Template</title>
+  <title>Business Frontpage - Start Bootstrap Template</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Bootstrap core CSS -->
+  <link href='./css/bootstrap.min.css' rel='stylesheet'>
 
-    <!-- Custom CSS -->
-    <link href="css/heroic-features.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+  <!-- Custom styles for this template -->
+  <link href='./css/business-frontpage.css' rel='stylesheet'>
 
 </head>
 
 <body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Start Bootstrap</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
+  <!-- Navigation -->
+  <nav class='navbar navbar-expand-lg navbar-dark bg-dark fixed-top'>
+    <div class='container'>
+      <a class='navbar-brand' href='#'>Start Bootstrap</a>
+      <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarResponsive' aria-controls='navbarResponsive' aria-expanded='false' aria-label='Toggle navigation'>
+        <span class='navbar-toggler-icon'></span>
+      </button>
+      <div class='collapse navbar-collapse' id='navbarResponsive'>
+        <ul class='navbar-nav ml-auto'>
+          <li class='nav-item active'>
+            <a class='nav-link' href='#'>Home
+              <span class='sr-only'>(current)</span>
+            </a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='#'>About</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='#'>Services</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='#'>Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Header -->
+  <header class='bg-primary py-5 mb-5'>
+    <div class='container h-100'>
+      <div class='row h-100 align-items-center'>
+        <div class='col-lg-12'>
+          <h1 class='display-4 text-white mt-5 mb-2'>Business Name or Tagline</h1>
+          <p class='lead mb-5 text-white-50'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non possimus ab labore provident mollitia. Id assumenda voluptate earum corporis facere quibusdam quisquam iste ipsa cumque unde nisi, totam quas ipsam.</p>
         </div>
-        <!-- /.container -->
-    </nav>
+      </div>
+    </div>
+  </header>
 
-    <!-- Page Content -->
-    <div class="container">
+  <!-- Page Content -->
+  <div class='container'>
 
-        <!-- Jumbotron Header -->
-        <header class="jumbotron hero-spacer">
-            <h1>A Warm Welcome!</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
-            <p><a class="btn btn-primary btn-large">Call to action!</a>
-            </p>
-        </header>
-
+    <div class='row'>
+      <div class='col-md-8 mb-5'>
+        <h2>What We Do</h2>
         <hr>
-
-        <!-- Title -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h3>Latest Features</h3>
-            </div>
-        </div>
-        <!-- /.row -->
-
-        <!-- Page Features -->
-        <div class="row text-center">
-
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="http://placehold.it/800x500" alt="">
-                    <div class="caption">
-                        <h3>Feature Label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        <p>
-                            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="http://placehold.it/800x500" alt="">
-                    <div class="caption">
-                        <h3>Feature Label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        <p>
-                            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="http://placehold.it/800x500" alt="">
-                    <div class="caption">
-                        <h3>Feature Label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        <p>
-                            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="http://placehold.it/800x500" alt="">
-                    <div class="caption">
-                        <h3>Feature Label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        <p>
-                            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-        <!-- /.row -->
-
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A deserunt neque tempore recusandae animi soluta quasi? Asperiores rem dolore eaque vel, porro, soluta unde debitis aliquam laboriosam. Repellat explicabo, maiores!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis optio neque consectetur consequatur magni in nisi, natus beatae quidem quam odit commodi ducimus totam eum, alias, adipisci nesciunt voluptate. Voluptatum.</p>
+        <a class='btn btn-primary btn-lg' href='#'>Call to Action &raquo;</a>
+      </div>
+      <div class='col-md-4 mb-5'>
+        <h2>Contact Us</h2>
         <hr>
+        <address>
+          <strong>Start Bootstrap</strong>
+          <br>3481 Melrose Place
+          <br>Beverly Hills, CA 90210
+          <br>
+        </address>
+        <address>
+          <abbr title='Phone'>P:</abbr>
+          (123) 456-7890
+          <br>
+          <abbr title='Email'>E:</abbr>
+          <a href='mailto:#'>name@example.com</a>
+        </address>
+      </div>
+    </div>
+    <!-- /.row -->
 
-        <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2030</p>
-                </div>
-            </div>
-        </footer>
+    <div class='row'>
+      <div class='col-md-4 mb-5'>
+        <div class='card h-100'>
+          <img class='card-img-top' src='http://placehold.it/300x200' alt=''>
+          <div class='card-body'>
+            <h4 class='card-title'>Card title</h4>
+            <p class='card-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+          </div>
+          <div class='card-footer'>
+            <a href='#' class='btn btn-primary'>Find Out More!</a>
+          </div>
+        </div>
+      </div>
+      <div class='col-md-4 mb-5'>
+        <div class='card h-100'>
+          <img class='card-img-top' src='http://placehold.it/300x200' alt=''>
+          <div class='card-body'>
+            <h4 class='card-title'>Card title</h4>
+            <p class='card-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus totam ut praesentium aut.</p>
+          </div>
+          <div class='card-footer'>
+            <a href='#' class='btn btn-primary'>Find Out More!</a>
+          </div>
+        </div>
+      </div>
+      <div class='col-md-4 mb-5'>
+        <div class='card h-100'>
+          <img class='card-img-top' src='http://placehold.it/300x200' alt=''>
+          <div class='card-body'>
+            <h4 class='card-title'>Card title</h4>
+            <p class='card-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+          </div>
+          <div class='card-footer'>
+            <a href='#' class='btn btn-primary'>Find Out More!</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /.row -->
 
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class='py-5 bg-dark'>
+    <div class='container'>
+      <p class='m-0 text-center text-white'>Copyright &copy; Your Website 2019</p>
     </div>
     <!-- /.container -->
+  </footer>
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script src='./js/jquery.min.js'></script>
+  <script src='./js/bootstrap.bundle.min.js'></script>
 
 </body>
 
