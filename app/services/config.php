3cb4e3c8865e4e45970359bf4268bb4b753777ac
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-27 05:41:46 -0500

    Update config.php

--- app/services/config.php
+++ app/services/config.php
@@ -3,7 +3,7 @@
 defined("DB_HOST") ? null : define("DB_HOST", "localhost");
 defined("DB_USER") ? null : define("DB_USER", "newuser");
 defined("DB_PASS") ? null : define("DB_PASS", "password");
-defined("DB_NAME") ? null : define("DB_NAME", "ecom_db_test");
+defined("DB_NAME") ? null : define("DB_NAME", "services");
 
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
