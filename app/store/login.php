675c9106ddf82be95c47b320084fbf011982def5
Ricky McAllister <rickymcallister45@gmail.com>
2019-11-27 03:16:44 -0500

    Update login.php

--- app/store/login.php
+++ app/store/login.php
@@ -9,14 +9,15 @@ echo "<body>
             <header>
               <h1 class='text-center'>Login</h1>
                 <div class='col-sm-4 col-sm-offset-5'>         
-                  <form class='' action='' method='post' enctype='multipart/form-data'>
-                    <div class='form-group'><label for=''>
-                      username<input type='text' name='username' class='form-control'></label>
+                  <form class='' action='' method='post' enctype='multipart/form-data'>";
+login_user();
+
+              echo "<div class='form-group'><label for=''>
+                        username<input type='text' name='username' class='form-control'></label>
                     </div>
                     <div class='form-group'><label for='password'>
-                      Password<input type='text' name='password' class='form-control'></label>
+                        Password<input type='text' name='password' class='form-control'></label>
                     </div>
-
                     <div class='form-group'>
                       <input type='submit' name='submit' class='btn btn-primary' >
                     </div>
