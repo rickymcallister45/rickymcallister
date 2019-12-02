3107e5e74f1903d41cfb7ea8c2522f75a0e1fe8a
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-02 08:36:33 -0500

    Update cart.php

--- app/store/cart.php
+++ app/store/cart.php
@@ -1,5 +1,5 @@
 <?php
-
+include_once "../../resources/store/config.php";
   if(isset($_GET['add'])) {
     $query - query("SELECT * FROM products WHERE product_id=" . escape_string($_GET['add']) . " ");
     confirm($query);
