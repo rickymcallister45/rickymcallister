6444ad5ed2858a773c58f8cc9baa64eb43f415b6
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-02 01:55:24 -0500

    Create cart.php

---
+++ app/store/frontEnd/cart.php
@@ -0,0 +1,36 @@
+<?php
+include_once "../../resources/store/config.php";
+
+  if(isset($_GET['add'])) {
+    $query - query("SELECT * FROM products WHERE product_id=" . escape_string($_GET['add']) . " ");
+    confirm($query);
+    
+    while($row = fetch_array($query)) {
+      if($row['product_quantity'] != $_SESSION['product_' . $_GET['add']]) {
+        $_SESSION['product_' . $_GET['add']] +=1;
+        redirect('./checkout.php');
+      }else{
+        set_message("We only have " . $row['product_quantity'] . " " . $row['product_limit_reached'] . " Available");
+        redirect('./checkout.php');
+      }
+    }
+    
+    //$_SESSION['product_' . $_GET['add']] +=1;
+    //redirect('./index.php');
+  }
+  
+  if(isset($_GET['remove'])) {
+    $_SESSION['product_' . $_GET['remove']] --;
+    if($_SESSION['product_' . $_GET['remove']] < 1) {
+      redirect('./checkout.php');
+    }else{
+      redirect('./checkout.php');
+    }
+  }
+  
+  if(isset($_GET['delete'])) {
+    $_SESSION['product_' . $_GET['delete']] == '0';
+    redirect('./checkout.php');
+  }
+  
+?>
