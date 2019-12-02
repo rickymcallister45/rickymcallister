4efc1d9b1d7d9ee3c6b43689cf3ada1bedaf5229
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-02 07:58:18 -0500

    Update config.php

--- resources/store/config.php
+++ resources/store/config.php
@@ -3,7 +3,7 @@
 ob_start();
 
 session_start();
-session_destroy();
+//session_destroy();
 
 defined("DB_HOST") ? null : define("DB_HOST", "localhost");
 defined("DB_USER") ? null : define("DB_USER", "newuser");
