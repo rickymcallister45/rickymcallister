c38b5cf8e111a073ee10968d9ecf3b8787748a1e
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-06 05:37:14 -0500

    Update adminSideBar.php

--- app/store/backEnd/adminSideBar.php
+++ app/store/backEnd/adminSideBar.php
@@ -3,7 +3,7 @@ echo "<!-- Sidebar Menu Items - These collapse to the responsive navigation menu
         <div class='collapse navbar-collapse navbar-ex1-collapse'>
           <ul class='nav navbar-nav side-nav'>
             <li class='active'>
-              <a href='index.html'><i class='fa fa-fw fa-dashboard'></i> Dashboard</a>
+              <a href='./index.php'><i class='fa fa-fw fa-dashboard'></i> Dashboard</a>
             </li>
             <li class=''>
               <a href='./orders.php'><i class='fa fa-fw fa-dashboard'></i> Orders</a>
