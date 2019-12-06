316544e94f6f3f2a72df17aa5adb0e52568b34e1
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-06 01:04:56 -0500

    Update thankYou.php

--- app/store/thankYou.php
+++ app/store/thankYou.php
@@ -2,6 +2,21 @@
 include_once '../../resources/store/test.php';
 include_once 'frontEnd/header.php';
 
+if(isset($_GET['tx'])) {
+        $ammount = $_GET['amt'];
+        $currency = $_GET['cc'];
+        $transaction =$_GET['tx'];
+        $status = $_GET['st'];
+        
+        $query = query("INSERT INTO orders (order_amount, order_transaction, order_status, order_currency)
+                                      VALUES('{$ammount}', '{$transaction}', '{$status}', '{$currency}')");
+        confirm($query);
+        
+        session_destroy();
+        
+}else{
+        redirect('./index.php');
+} 
 
 echo "<body>";
         include_once 'frontEnd/topNav.php';
@@ -10,12 +25,9 @@ echo "<body>";
             <div class='row'>
               <!-- Side Nav -->";
                 include_once 'frontEnd/sideNav.php';
-                  echo "<div class='col-md-9'>
-              <!-- Slider -->";
-                include_once 'frontEnd/slider.php';
-                  echo "<!-- Products -->";
-                include_once 'frontEnd/products.php';
-                  echo "</div>
+         echo "<div class='col-md-9'>
+                 <h1 class='text-center'>THANK YOU!!</h1>
+               </div>
           </div>
     </div>";
 include_once 'frontEnd/footer.php';
