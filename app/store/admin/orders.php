2ed9b27205e2ce7e0880dd67446813e53e903e50
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-06 23:57:08 -0500

    Update orders.php

--- app/store/admin/orders.php
+++ app/store/admin/orders.php
@@ -1,10 +1,5 @@
 <?php
-include_once '../../../resources/store/config.php';
-include_once '../backEnd/header.php';
-
-echo "<div id='page-wrapper'>
-        <div class='container-fluid'>
-          <div class='col-md-12'>
+    echo "<div class='col-md-12'>
             <div class='row'>
               <h1 class='page-header'>
                 All Orders
@@ -23,26 +18,19 @@ echo "<div id='page-wrapper'>
                     <th>Status</th>
                   </tr>
                 </thead>
-              <tbody>
-            <tr>
-              <td>21</td>
-              <td>Nikon 234</td>
-              <td><img src='http://placehold.it/62x62' alt=''></td>
-              <td>Cameras</td>
-              <td>456464</td>
-              <td>Jun 2039</td>
-              <td>Completed</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-    <!-- /.container-fluid -->
-  </div>
-  <!-- /#page-wrapper -->
-</div>
-<!-- /#wrapper -->";
+                <tbody>
+                  <tr>
+                    <td>21</td>
+                    <td>Nikon 234</td>
+                    <td><img src='http://placehold.it/62x62' alt=''></td>
+                    <td>Cameras</td>
+                    <td>456464</td>
+                    <td>Jun 2039</td>
+                    <td>Completed</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>";
 
-include_once '../backEnd/footer.php';
-
-   ?>
+?>
