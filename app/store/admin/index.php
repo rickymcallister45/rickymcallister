92a3d25eea8063d00dee8ee5ee33a5a366339e3c
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-07 00:50:19 -0500

    Update index.php

--- app/store/admin/index.php
+++ app/store/admin/index.php
@@ -3,29 +3,16 @@ include_once '../../../resources/store/config.php';
 include_once '../backEnd/header.php';
 
    echo "<div id='page-wrapper'>
-           <div class='container-fluid'>
-             <!-- Page Heading -->
-               <div class='row'>
-                 <div class='col-lg-12'>
-                   <h1 class='page-header'>
-                     Dashboard <small>Statistics Overview</small>
-                   </h1>
-                   <ol class='breadcrumb'>
-                     <li class='active'>
-                       <i class='fa fa-dashboard'></i> Dashboard
-                     </li>
-                   </ol>
-                 </div>
-               </div>
-             <!-- /.row -->";
+           <div class='container-fluid'>;
+             
 echo $_SERVER['REQUEST_URI'];
 
 if($_SERVER['REQUEST_URI'] == 'app/store/admin/' || $_SERVER['REQUEST_URI'] == 'app/store/admin/index.php') {
-   include '../backEnd/adminPageContent.php';
+   include '../backEnd/adminDashboardContent.php';
 }
 
 if(isset($_GET['orders'])) {
-   include('../backEnd/orders.php');
+   include('../backEnd/adminOrders.php');
 }
    
    echo "</div>
