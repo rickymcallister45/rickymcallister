620ed5c02774e536a3c2d55f450323aefe1aba8e
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-07 01:23:39 -0500

    Update index.php

--- app/store/admin/index.php
+++ app/store/admin/index.php
@@ -2,8 +2,12 @@
 include_once '../../../resources/store/config.php';
 include_once '../backEnd/header.php';
 
+if(!isset($_SESSION['username'])) {
+   redirect('../index.php');
+}
+
    echo "<div id='page-wrapper'>
-           <div class='container-fluid'>;
+           <div class='container-fluid'>";
              
 echo $_SERVER['REQUEST_URI'];
 
@@ -14,6 +18,23 @@ if($_SERVER['REQUEST_URI'] == 'app/store/admin/' || $_SERVER['REQUEST_URI'] == '
 if(isset($_GET['orders'])) {
    include('../backEnd/adminOrders.php');
 }
+
+if(isset($_GET['products'])) {
+   include('../backEnd/adminProducts.php');
+}
+
+if(isset($_GET['addProducts'])) {
+   include('../backEnd/adminAddProducts.php');
+}
+
+if(isset($_GET['categories'])) {
+   include('../backEnd/adminCategories.php');
+}
+
+if(isset($_GET['users'])) {
+   include('../backEnd/adminUsers.php');
+}
+
    
    echo "</div>
          <!-- /.container-fluid -->
