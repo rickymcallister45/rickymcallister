d03583f1bdd397cb8a8a0604925a925477f1798f
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-07 02:15:49 -0500

    Update item.php

--- app/store/item.php
+++ app/store/item.php
@@ -39,7 +39,7 @@ $query = query("SELECT * FROM products WHERE product_id =" . escape_string($_GET
 
                         <form action=''>
                           <div class='form-group'>
-                            <a href='./cart.php?add={$row['product_id']}' class='btn btn-primary'>ADD TO CART</a>
+                            <a href='./frontEnd/cart.php?add={$row['product_id']}' class='btn btn-primary'>ADD TO CART</a>
                           </div>
                         </form>
                       </div>
