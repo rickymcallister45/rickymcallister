2ebdaf65199341b9c739ecf41f09f68a2c6fe427
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-07 00:29:08 -0500

    Update adminPageContent.php

--- app/store/backEnd/adminPageContent.php
+++ app/store/backEnd/adminPageContent.php
@@ -1,5 +1,19 @@
 <?php
-echo "<!-- FIRST ROW WITH PANELS -->
+echo "<!-- Page Heading -->
+               <div class='row'>
+                 <div class='col-lg-12'>
+                   <h1 class='page-header'>
+                     Dashboard <small>Statistics Overview</small>
+                   </h1>
+                   <ol class='breadcrumb'>
+                     <li class='active'>
+                       <i class='fa fa-dashboard'></i> Dashboard
+                     </li>
+                   </ol>
+                 </div>
+               </div>
+             <!-- /.row -->
+<!-- FIRST ROW WITH PANELS -->
 
                 <!-- /.row -->
                 <div class='row'>
