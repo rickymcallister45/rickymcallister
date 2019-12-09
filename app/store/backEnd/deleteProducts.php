4d910a461c6a3263ea7414faa1f6dc7ece7ca740
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-09 00:03:10 -0500

    Update deleteProducts.php

--- app/store/backEnd/deleteProducts.php
+++ app/store/backEnd/deleteProducts.php
@@ -1,11 +1,12 @@
 <?php
 include_once '../../../resources/store/config.php';
 include_once '../../../resources/store/functions.php';
+
 if(isset($_GET['id'])) {
-  $deleteOrderQuery = query("DELETE FROM orders WHERE order_id =" . escape_string($_GET['id']) . "");
-  confirm($deleteOrderQuery);
-  redirect("../admin/index.php?orders");
+  $deleteProductQuery = query("DELETE FROM products WHERE product_id =" . escape_string($_GET['id']) . "");
+  confirm($deleteProductQuery);
+  redirect("../admin/index.php?products");
 }else{
-  redirect("../admin/index.php?orders");
+  redirect("../admin/index.php?products");
 }
 ?>
