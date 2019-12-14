778977675b225c3e8f0599bade0a19b8491289e0
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-14 06:22:24 -0500

    Update adminAddUser.php

--- app/store/backEnd/adminAddUser.php
+++ app/store/backEnd/adminAddUser.php
@@ -21,14 +21,6 @@ echo "<h1 class='page-header'>
             <input type='text' name='email' class='form-control'>
           </div>
           <div class='form-group'>
-            <label for='first name'>First Name</label>
-            <input type='text' name='first_name' class='form-control'>
-          </div> 
-          <div class='form-group'>
-            <label for='last name'>Last Name</label>
-            <input type='text' name='last_name' class='form-control'>
-          </div>
-          <div class='form-group'>
             <label for='password'>Password</label>
             <input type='password' name='password' class='form-control'>
           </div>
