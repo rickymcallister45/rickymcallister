39c97970f3f17007bfe71353d8084da8b453eb35
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-14 02:19:53 -0500

    Update deleteUser.php

--- app/store/backEnd/deleteUser.php
+++ app/store/backEnd/deleteUser.php
@@ -1,27 +1,16 @@
-<?php require_once("../../resources/config.php");
+<?php 
+
+include_once '../../../resources/store/config.php';
+include_once '../../../resources/store/functions.php';
 
 
 if(isset($_GET['delete_user_id'])) {
-
-
-$query = query("DELETE FROM users WHERE user_id = " . escape_string($_GET['delete_user_id']) . " ");
-confirm($query);
-
-
-set_message("Sser Deleted");
-redirect("./index.php?users");
-
-
-} else {
-
-redirect("./index.php?users");
-
-
+  $deleteUserQuery = query("DELETE FROM users WHERE user_id = " . escape_string($_GET['delete_user_id']) . " ");
+  confirm($deleteUserQuery);
+  redirect("./index.php?users");
+  
+}else{
+  redirect("./index.php?users");
 }
-
-
-
-
-
 
  ?>
