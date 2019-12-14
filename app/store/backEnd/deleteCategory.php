68628ccb0cf3587cd73c79623dbd9b549c9376e5
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-14 00:50:56 -0500

    Update deleteCategory.php

--- app/store/backEnd/deleteCategory.php
+++ app/store/backEnd/deleteCategory.php
@@ -2,10 +2,10 @@
 include_once '../../../resources/store/config.php';
 include_once '../../../resources/store/functions.php';
 if(isset($_GET['id'])) {
-  $deleteOrderQuery = query("DELETE FROM orders WHERE order_id =" . escape_string($_GET['id']) . "");
-  confirm($deleteOrderQuery);
-  redirect("../admin/index.php?orders");
+  $deleteCategoryQuery = query("DELETE FROM categories WHERE category_id =" . escape_string($_GET['id']) . "");
+  confirm($deleteCategoryQuery);
+  redirect("../admin/index.php?categories");
 }else{
-  redirect("../admin/index.php?orders");
+  redirect("../admin/index.php?categories");
 }
 ?>
