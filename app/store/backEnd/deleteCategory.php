fb69820a9ea636903f3510a51a8d7fc138ad3843
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-14 07:50:11 -0500

    Update deleteCategory.php

--- app/store/backEnd/deleteCategory.php
+++ app/store/backEnd/deleteCategory.php
@@ -1,6 +1,7 @@
 <?php
 include_once '../../../resources/store/config.php';
 include_once '../../../resources/store/functions.php';
+
 if(isset($_GET['id'])) {
   $deleteCategoryQuery = query("DELETE FROM categories WHERE category_id =" . escape_string($_GET['id']) . "");
   confirm($deleteCategoryQuery);
