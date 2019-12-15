3f987e1805619cbedd75c2f10b07121bdf7d4235
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-15 02:08:29 -0500

    Update adminReports.php

--- app/store/backEnd/adminReports.php
+++ app/store/backEnd/adminReports.php
@@ -1,9 +1,115 @@
 <?php
+countOrders();
+countProducts();
+countCategories();
+
 echo "<div class='row'>
-        <h1 class='page-header'>
-          All Products
-        </h1>
-        <table class='table table-hover'>
+        <div class='col-lg-12'>
+          <h1 class='page-header'>
+            Dashboard <small>Statistics Overview</small>
+          </h1>
+          <ol class='breadcrumb'>
+            <li class='active'>
+              <i class='fa fa-dashboard'></i> Dashboard
+            </li>
+          </ol>
+        </div>
+      </div>
+      <!-- /.row -->
+      <!-- FIRST ROW WITH PANELS -->
+      <div class='row'>
+        <div class='col-lg-4 col-md-6'>
+          <div class='panel panel-yellow'>
+            <div class='panel-heading'>
+              <div class='row'>
+                <div class='col-xs-3'>
+                  <i class='fa fa-shopping-cart fa-5x'></i>
+                </div>
+                <div class='col-xs-9 text-right'>
+                  <div class='huge'>";
+
+echo $orderCount;
+
+            echo "</div>
+                  <div>New Orders!</div>
+                </div>
+              </div>
+            </div>
+            <a href='../backEnd/adminOrders.php'>
+              <div class='panel-footer'>
+                <span class='pull-left'>View Details</span>
+                <span class='pull-right'><i class='fa fa-arrow-circle-right'></i></span>
+                <div class='clearfix'></div>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class='col-lg-4 col-md-6'>
+          <div class='panel panel-red'>
+            <div class='panel-heading'>
+              <div class='row'>
+                <div class='col-xs-3'>
+                  <i class='fa fa-support fa-5x'></i>
+                </div>
+                <div class='col-xs-9 text-right'>
+                  <div class='huge'>";
+
+echo $productCount;
+
+            echo "</div>
+                  <div>Products!</div>
+                </div>
+              </div>
+            </div>
+            <a href='../backEnd/adminProducts.php'>
+              <div class='panel-footer'>
+                <span class='pull-left'>View Details</span>
+                <span class='pull-right'><i class='fa fa-arrow-circle-right'></i></span>
+                <div class='clearfix'></div>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class='col-lg-4 col-md-6'>
+          <div class='panel panel-green'>
+            <div class='panel-heading'>
+              <div class='row'>
+                <div class='col-xs-3'>
+                  <i class='fa fa-tasks fa-5x'></i>
+                </div>
+                <div class='col-xs-9 text-right'>
+                  <div class='huge'>";
+
+echo $countCategories();
+
+            echo "</div>
+                  <div>Categories!</div>
+                </div>
+              </div>
+            </div>
+            <a href='../backEnd/adminCategories.php'>
+              <div class='panel-footer'>
+                <span class='pull-left'>View Details</span>
+                <span class='pull-right'><i class='fa fa-arrow-circle-right'></i></span>
+                <div class='clearfix'></div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- /.row -->
+      <!-- SECOND ROW WITH TABLES-->
+      <div class='row'>
+        <div class='col-lg-6'>
+          <div class='panel panel-default'>
+            <div class='panel-heading'>
+              <h3 class='panel-title'>
+                <i class='fa fa-money fa-fw'></i> Transactions Panel
+              </h3>
+            </div>
+            <div class='panel-body'>
+              <div class='table-responsive'>
+                <table class='table table-bordered table-hover table-striped'>
           <thead>
             <tr>
               <th>Id</th>
@@ -20,5 +126,8 @@ adminGetReports();
 
     echo "</tbody>
         </table>
-      </div>";
+      </div>
+    </div>
+  </div>
+</div>";
 ?>
