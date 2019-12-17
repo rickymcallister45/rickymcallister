f2b15ba4d6093a4f5134c2e69bb29129fffdbb19
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-17 03:21:57 -0500

    Update category.php

--- app/store/category.php
+++ app/store/category.php
@@ -7,19 +7,20 @@ echo "<!-- Page Content -->
        <div class='container'>
 
         <!-- Jumbotron Header -->
+        /*
         <header class='jumbotron hero-spacer'>
             <h1>A Warm Welcome!</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
             <p><a class='btn btn-primary btn-large'>Call to action!</a>
             </p>
         </header>
-
+        */
         <hr>
 
         <!-- Title -->
         <div class='row'>
             <div class='col-lg-12'>
-                <h3>Latest Features</h3>
+                <h3>Current Products</h3>
             </div>
         </div>
         <!-- /.row -->
