de5926db1f96b47ee6306f8c1e6ed3aa16bd0a86
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-26 02:14:17 -0500

    Update and rename index.html to index.php

---
+++ app/services/lockpicking/index.php
@@ -0,0 +1,21 @@
+<?php
+
+include "../resources/head.php";
+include "../resources/nav.php";
+
+echo "<body>";
+
+if($_SERVER['REQUEST_URI'] == '/app/services/lockpicking/index' || $_SERVER['REQUEST_URI'] == '/app/services/lockpicking/index.php') {
+   include './php/lockpicking.php';
+}
+
+if(isset($_GET['contact'])) {
+   include('../resources/contact.php');
+}
+
+
+
+echo "</body>";
+
+
+?>
