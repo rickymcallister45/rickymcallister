c0f487a00828b89fab6c71982b9cd771d25ef048
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-26 01:40:59 -0500

    Create head.php

---
+++ app/services/resources/head.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "<!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <meta name="description" content="Ricky McAllister's Services">
+            <meta name="author" content="Ricky McAllister">
+
+            <title>rickyMcAllister</title>
+            <link rel='icon' href='../../resources/favicon/favicon.ico' type='image/x-icon'>            
+
+            <link href="..//css/bootstrap.min.css" rel="stylesheet">
+            <link href="../css/grayscale.min.css" rel="stylesheet">
+            
+            <link href="css/style.css" rel="stylesheet">
+</head>";
+
+?>
