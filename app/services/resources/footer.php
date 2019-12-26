125a19c271393768559d528ca1367b2d378020ff
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-26 02:30:46 -0500

    Create footer.php

---
+++ app/services/resources/footer.php
@@ -0,0 +1,14 @@
+<?php
+echo "<footer class='bg-black small text-center text-white-50'>
+    <div class='container'>
+      Copyright &copy; rickyMcAllister.com 2019
+    </div>
+  </footer>
+
+  <script src='vendor/jquery/jquery.min.js'></script>
+  <script src='vendor/bootstrap/js/bootstrap.bundle.min.js'></script>
+  
+  <script src='vendor/jquery-easing/jquery.easing.min.js'></script>
+
+  <script src='js/grayscale.min.js'></script>";
+?>
