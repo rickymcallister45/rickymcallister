e87cbb0416b813b358dbbd3843076198bc2f8640
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-26 02:38:26 -0500

    Update footer.php

--- app/services/resources/footer.php
+++ app/services/resources/footer.php
@@ -5,10 +5,10 @@ echo "<footer class='bg-black small text-center text-white-50'>
     </div>
   </footer>
 
-  <script src='vendor/jquery/jquery.min.js'></script>
-  <script src='vendor/bootstrap/js/bootstrap.bundle.min.js'></script>
+  <script src='../jquery.min.js'></script>
+  <script src='../js/bootstrap.bundle.min.js'></script>
   
-  <script src='vendor/jquery-easing/jquery.easing.min.js'></script>
+  <script src='..//jquery.easing.min.js'></script>
 
-  <script src='js/grayscale.min.js'></script>";
+  <script src='../js/grayscale.min.js'></script>";
 ?>
