010f4fc692d910e54e6eee2e32e7197b694a5987
Ricky McAllister <rickymcallister45@gmail.com>
2019-12-26 06:34:06 -0500

    Update script.js

--- app/services/lockpicking/js/script.js
+++ app/services/lockpicking/js/script.js
@@ -4,24 +4,16 @@ $(document).ready(function(){
 	
 	
 	var key = 'AIzaSyATpzdfvkMW6jt9rRpj99Dp57LwW77nxMA';
-	var playlistID = 'PL3Rb7VW4urJd-46ZScwJvex5WMDnbN1-R';
+	var playlistID = 'PL3Rb7VW4urJe2yqaVf-9pwvGPUINaf5Ov';
 	var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
 	var options = {
 		part: 'snippet',
 		key: key,
-		maxResults: 20,
+		maxResults: 100,
 		playlistId: playlistID
 	}
 
-	$(".menu-icon").click(function(){
- 				$(".menu-icon").toggleClass("active")
- 			})
- 			$(".menu-icon").click(function(){
- 				$(".sidebar").toggleClass("active")
- 			})
- 			$(".menu-icon").click(function(){
- 				$(".sidebarLeft").toggleClass("activeLeft")
- 			})
+	
 
 loadVids();
 
@@ -29,43 +21,25 @@ function loadVids() {
 	$.getJSON(URL, options, function(data){
 		console.log(data);
 		var id = data.items[0].snippet.resourceId.videoId;
-		mainVid(id);
 		resultsLoop(data);
 	})
-}
-
-function mainVid(id) {
-	$('#video'). html(`
-		<iframe width="1100" height="800" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-		`);
 }
 
 function resultsLoop(data) {
 
 	$.each(data.items, function(i, item) {
-
+		var youtubePath = 'https://www.youtube.com/watch?v=';
 		var thumb = item.snippet.thumbnails.medium.url;
 		var title = item.snippet.title;
 		var description = item.snippet.description.substring(0,100);
 		var date = item.snippet.publishedAt.substring(0,10);
 		var vid = item.snippet.resourceId.videoId;
+		var completePath = youtubePath + vid
 
-		$('main').append(`
-		<article class='target' data-key="${vid}">
-			<img src="${thumb}" class="thumb">
-			<div class="details">
-				<h4>${title}</h4>
-				<p>${description}</p>
-				
-			</div>
-		</article>
-		`);
+		$('.lockpickingVideos').append('<a href="' + completePath +  '">')
+			  		.append('<img src="' + thumb + '" class="videoStyling"></a>');
 	});
 }
 
-$('main').on('click', 'article', function(){
-			var waka = $(this).attr('data-key');
-			mainVid(waka);			
-		});
 
 });
