bfd2f269c59049f7f32b650d80050ff3700f7e21
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-04 02:20:04 -0500

    Update and rename index.html to index.php

---
+++ app/services/carDetailing/index.php
@@ -0,0 +1,14 @@
+<?php
+include "../resources/head.php";
+include "../resources/nav.php";
+echo "<body>";
+if($_SERVER['REQUEST_URI'] == '/app/services/carDetailing
+/index' || $_SERVER['REQUEST_URI'] == '/app/services/carDetailing/index.php') {
+   include './php/carDetailing.php';
+}
+if(isset($_GET['contact'])) {
+   include('../resources/contact.php');
+}
+include "../resources/footer.php";
+echo "</body>";
+?>
