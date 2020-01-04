68be09e418c9919756282f9392a1c00832ffd338
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-04 00:18:17 -0500

    Update index.php

--- app/services/juicer/index.php
+++ app/services/juicer/index.php
@@ -1,6 +1,13 @@
 <?php
-
-echo "Coming soon";
-
-
+include "../resources/head.php";
+include "../resources/nav.php";
+echo "<body>";
+if($_SERVER['REQUEST_URI'] == '/app/services/juicer/index' || $_SERVER['REQUEST_URI'] == '/app/services/juicer/index.php') {
+   include './php/juicer.php';
+}
+if(isset($_GET['contact'])) {
+   include('../resources/contact.php');
+}
+include "../resources/footer.php";
+echo "</body>";
 ?>
