332a6b380f992e9d436e946944bfd5706b04c8b1
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-04 04:06:46 -0500

    Update nav.php

--- app/services/resources/nav.php
+++ app/services/resources/nav.php
@@ -1,4 +1,5 @@
 <?php
+$folderName = basename(dirname(__FILE__));
 
 echo "<nav class='navbar navbar-expand-lg navbar-light fixed-top' id='mainNav'>
         <div class='container'>
@@ -11,6 +12,9 @@ echo "<nav class='navbar navbar-expand-lg navbar-light fixed-top' id='mainNav'>
             <ul class='navbar-nav ml-auto'>
               <li class='nav-item'>
                 <a class='nav-link js-scroll-trigger' href='../../services/index.php'>Services</a>
+              </li>
+              <li class='nav-item'>
+                <a class='nav-link js-scroll-trigger' href='./index.php'>{$folderName}</a>
               </li>
               <li class='nav-item'>
                 <a class='nav-link js-scroll-trigger' href='./index.php?contact'>Contact</a>
