861625876d271da57b774a4cf2176574eee06027
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-04 02:15:09 -0500

    Update index.php

--- app/services/barista/index.php
+++ app/services/barista/index.php
@@ -1,3 +1,13 @@
 <?php
-echo "cooming Soon";
+include "../resources/head.php";
+include "../resources/nav.php";
+echo "<body>";
+if($_SERVER['REQUEST_URI'] == '/app/services/barista/index' || $_SERVER['REQUEST_URI'] == '/app/services/barista/index.php') {
+   include './php/barista.php';
+}
+if(isset($_GET['contact'])) {
+   include('../resources/contact.php');
+}
+include "../resources/footer.php";
+echo "</body>";
 ?>
