ca2d7e1b4c714350752ac30685cef45fd6f5bb99
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-04 02:15:34 -0500

    Create barista.php

---
+++ app/services/barista/php/barista.php
@@ -0,0 +1,11 @@
+<?php
+echo "<header class='masthead'>
+        <div class='container d-flex h-100 align-items-center'>
+          <div class='pageContent'>Coming Soon!!    
+          </div>
+        </div>
+      </header>";
+$folderName = basename(dirname(__FILE__));
+
+echo $folderName;
+?>
