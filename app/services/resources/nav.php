0f85ffe798bb7c366887a87cbf23e530560f631d
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-04 07:57:45 -0500

    Update nav.php

--- app/services/resources/nav.php
+++ app/services/resources/nav.php
@@ -1,5 +1,5 @@
 <?php
-$folderName = basename(dirname(__FILE__));
+$folderName = dirname(__FILE__);
 
 echo "<nav class='navbar navbar-expand-lg navbar-light fixed-top' id='mainNav'>
         <div class='container'>
