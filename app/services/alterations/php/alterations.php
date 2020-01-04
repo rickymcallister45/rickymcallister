9e6f1e4e5f9239521850aaa63973e905417a28e3
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-03 23:53:48 -0500

    Update alterations.php

--- app/services/alterations/php/alterations.php
+++ app/services/alterations/php/alterations.php
@@ -1,11 +1,12 @@
 <?php
 
-/* <body style="background-image: url(./alterations.jpg); background-size: cover;">
-	<div style="color: white; background-color: #f0f5f5; text-align: center; height: 100px; line-height: 100px;">
-		<a href="mailto:rickymcallister45@gmail.com">requestBid</a>
-	</div>
-	
-</body>
-
-*/
+echo "<header class='masthead'>
+        <div class='container d-flex h-100 align-items-center'>
+          <div style='background-image: url(./alterations.jpg); background-size: cover;'>
+	    <div style='color: white; background-color: #f0f5f5; text-align: center; height: 100px; line-height: 100px;'>
+ 	      <a href='mailto:rickymcallister45@gmail.com'>requestBid</a>
+	    </div>
+          </div>
+        </div>
+      </header>";
 ?>
