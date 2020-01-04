062972a3a4d9f9005d2c6700b7af87eb2e2e1d5a
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-03 23:50:02 -0500

    Create alterations.php

---
+++ app/services/alterations/php/alterations.php
@@ -0,0 +1,11 @@
+<?php
+
+/* <body style="background-image: url(./alterations.jpg); background-size: cover;">
+	<div style="color: white; background-color: #f0f5f5; text-align: center; height: 100px; line-height: 100px;">
+		<a href="mailto:rickymcallister45@gmail.com">requestBid</a>
+	</div>
+	
+</body>
+
+*/
+?>
