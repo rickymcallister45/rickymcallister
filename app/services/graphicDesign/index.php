94ecb7bdc2dc8d86ebf18d923bcd57d499dc7a3a
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-04 04:02:11 -0500

    Update index.php

--- app/services/graphicDesign/index.php
+++ app/services/graphicDesign/index.php
@@ -1,3 +1,13 @@
 <?php
-echo "Coming Soon";
+include "../resources/head.php";
+include "../resources/nav.php";
+echo "<body>";
+if($_SERVER['REQUEST_URI'] == '/app/services/graphicDesign/index' || $_SERVER['REQUEST_URI'] == '/app/services/graphicDesign/index.php') {
+   include './php/graphicDesign.php';
+}
+if(isset($_GET['contact'])) {
+   include('../resources/contact.php');
+}
+include "../resources/footer.php";
+echo "</body>";
 ?>
