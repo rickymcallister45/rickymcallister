bc1a18eb8913fa65f3ae84e2b45f9ef5fb7b1eae
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-04 02:20:23 -0500

    Update index.php

--- app/services/carDetailing/index.php
+++ app/services/carDetailing/index.php
@@ -2,8 +2,7 @@
 include "../resources/head.php";
 include "../resources/nav.php";
 echo "<body>";
-if($_SERVER['REQUEST_URI'] == '/app/services/carDetailing
-/index' || $_SERVER['REQUEST_URI'] == '/app/services/carDetailing/index.php') {
+if($_SERVER['REQUEST_URI'] == '/app/services/carDetailing/index' || $_SERVER['REQUEST_URI'] == '/app/services/carDetailing/index.php') {
    include './php/carDetailing.php';
 }
 if(isset($_GET['contact'])) {
