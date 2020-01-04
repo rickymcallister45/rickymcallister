82f8a1098cac30052d8d13778ac289769c67fb29
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-04 02:13:37 -0500

    Update index.php

--- app/services/animations/index.php
+++ app/services/animations/index.php
@@ -1,3 +1,13 @@
 <?php
-echo "Coming Soon";
+include "../resources/head.php";
+include "../resources/nav.php";
+echo "<body>";
+if($_SERVER['REQUEST_URI'] == '/app/services/animations/index' || $_SERVER['REQUEST_URI'] == '/app/services/animations/index.php') {
+   include './php/animations.php';
+}
+if(isset($_GET['contact'])) {
+   include('../resources/contact.php');
+}
+include "../resources/footer.php";
+echo "</body>";
 ?>
