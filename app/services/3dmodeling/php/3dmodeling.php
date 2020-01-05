d9afe61b1026253c3e2d056cef6a5b0200133541
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-05 01:56:27 -0500

    Delete 3dmodeling.php

--- app/services/3dmodeling/php/3dmodeling.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "<header class='masthead'>
-        <div class='container d-flex h-100 align-items-center'>
-          <div class='pageContent'>Coming Soon!!    
-          </div>
-        </div>
-      </header>";
-?>
