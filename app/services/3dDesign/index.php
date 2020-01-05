c2f6d3a9fd930468a0de0a4e47f17f99cf310349
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-05 01:54:09 -0500

    Update and rename app/services/3dmodeling/index.php to app/services/3dDesign/index.php

--- app/services/3dDesign/index.php
+++ app/services/3dDesign/index.php
@@ -2,8 +2,8 @@
 include "../resources/head.php";
 include "../resources/nav.php";
 echo "<body>";
-if($_SERVER['REQUEST_URI'] == '/app/services/3dmodeling/index' || $_SERVER['REQUEST_URI'] == '/app/services/3dmodeling/index.php') {
-   include './php/3dmodeling.php';
+if($_SERVER['REQUEST_URI'] == '/app/services/3dDesign/index' || $_SERVER['REQUEST_URI'] == '/app/services/3dDesign/index.php') {
+   include './php/3dDesign.php';
 }
 if(isset($_GET['contact'])) {
    include('../resources/contact.php');
