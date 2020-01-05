fcf306f47ee11b496bea1d60d73b8b7f78b8cc16
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-05 02:50:19 -0500

    Update index.php

--- app/services/bartender/index.php
+++ app/services/bartender/index.php
@@ -1,8 +1,13 @@
 <?php
-
-echo "Coming Soon";
-
-
-echo "<a href='./pdf/professionalbartender.pdf'><img src='./img/barGlassGuide.jpg'></a>";
-
+include "../resources/head.php";
+include "../resources/nav.php";
+echo "<body>";
+if($_SERVER['REQUEST_URI'] == '/app/services/bartender/index' || $_SERVER['REQUEST_URI'] == '/app/services/bartender/index.php') {
+   include './php/bartender.php';
+}
+if(isset($_GET['contact'])) {
+   include('../resources/contact.php');
+}
+include "../resources/footer.php";
+echo "</body>";
 ?>
