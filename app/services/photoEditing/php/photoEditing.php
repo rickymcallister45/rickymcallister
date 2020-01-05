45fc8b881ececfe194c88fac421e4adff20fc78a
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-05 01:32:26 -0500

    Create photoEditing.php

---
+++ app/services/photoEditing/php/photoEditing.php
@@ -0,0 +1,8 @@
+<?php
+echo "<header class='masthead'>
+        <div class='container d-flex h-100 align-items-center'>
+          <div class='pageContent'>Coming Soon!!    
+          </div>
+        </div>
+      </header>";
+?>
