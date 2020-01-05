dc3c6fdf051ca28ca0b53b07598d6235f73e0d64
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-05 02:52:06 -0500

    Update index.php

--- app/services/dryCleaning/index.php
+++ app/services/dryCleaning/index.php
@@ -1,3 +1,21 @@
 <?php
-echo "Coming Soon";
+
+$folderName = $_SERVER['REQUEST_URI'];
+$regexForDirectoryName = "/(\/app\/services\/)(.*)(\/index)/";
+preg_match($regexForDirectoryName, $folderName, $matches);
+$navItemName = json_encode($matches['2']);
+
+
+include "../resources/head.php";
+include "../resources/nav.php";
+echo "<body>";
+if($_SERVER['REQUEST_URI'] == '/app/services/{$navItemName}/index' || $_SERVER['REQUEST_URI'] == '/app/services/{$navItemName}/index.php') {
+   include './php/{$navItemName}.php';
+}
+if(isset($_GET['contact'])) {
+   include('../resources/contact.php');
+}
+include "../resources/footer.php";
+echo "</body>";
 ?>
+
