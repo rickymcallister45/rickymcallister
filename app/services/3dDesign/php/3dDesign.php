09e90b049c85f581659be8a67f999c8c404ce284
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-05 23:57:47 -0500

    Update 3dDesign.php

--- app/services/3dDesign/php/3dDesign.php
+++ app/services/3dDesign/php/3dDesign.php
@@ -1,6 +1,8 @@
-.masthead {
-  background: url(../img/mainBackground.jpg);
-}
-.signup-section {
-  background: url(../img/contactBackground.jpg);
-}
+<?php
+echo "<header class='masthead'>
+        <div class='container d-flex h-100 align-items-center'>
+          <h1>coming Soon!!!</h1>
+          </div>
+        </div>
+      </header>";
+?>
