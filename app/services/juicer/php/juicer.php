6f02a03e4406472e537a2d5a060b5351d8dd837f
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-06 01:31:52 -0500

    Update juicer.php

--- app/services/juicer/php/juicer.php
+++ app/services/juicer/php/juicer.php
@@ -1,7 +1,11 @@
 <?php
 echo "<header class='masthead'>
         <div class='container d-flex h-100 align-items-center'>
-          <div class='pageContent'>Coming Soon!!    
+          <div class='pageContent'>
+          
+            <a href='./img/appleBlueberrySmoothie1.jpg'><img src='./img/appleBlueberrySmoothie.jpg'>
+            </a>
+          
           </div>
         </div>
       </header>";
