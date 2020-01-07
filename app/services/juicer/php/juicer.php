5d8ad5d2f002c860737bbfb4d0785d2ab226801c
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-07 01:15:40 -0500

    Update juicer.php

--- app/services/juicer/php/juicer.php
+++ app/services/juicer/php/juicer.php
@@ -14,6 +14,17 @@ echo "<header class='masthead'>
                   </div>
                 </div>  
               </div>
+              
+              <div class='col-sm-4 col-lg-4 col-md-4'>
+                <div class='thumbnail'>
+                  <a href='./img/bloodyMary.jpg'><img style='height: 175px;' src='./img/bloodyMary.jpg' alt='bloodyMary'></a>
+                  <div class='caption'>
+                    <h4><a width='150' height='350' href='./img/bloodyMary.jpg'>bloodyMary</a></h4>
+                    <p>bloodyMary</p>
+                  </div>
+                </div>  
+              </div>
+              
             </div>
             
             
@@ -22,13 +33,34 @@ echo "<header class='masthead'>
               <h1>Juices</h1>
               <div class='col-sm-4 col-lg-4 col-md-4'>
                 <div class='thumbnail'>
-                  <a href='./img/appleBlueberrySmoothie1.jpg'><img style='height: 175px;' src='./img/appleBlueberrySmoothie.jpg' alt='appleBlueberrySmoothie'></a>
+                  <a href='./img/carrotJuice.jpg'><img style='height: 175px;' src='./img/carrotJuice.jpg' alt='carrotJuice'></a>
                   <div class='caption'>
-                    <h4><a width='150' height='350' href='./img/appleBlueberrySmoothie1.jpg'>Apple, Blueberry, and Banana</a></h4>
-                    <p>Apple, Blueberry, and Banana Smoothie</p>
+                    <h4><a width='150' height='350' href='./img/carrotJuice.jpg'>carrotJuice</a></h4>
+                    <p>carrotJuice</p>
                   </div>
                 </div>  
               </div>
+              
+              <div class='col-sm-4 col-lg-4 col-md-4'>
+                <div class='thumbnail'>
+                  <a href='./img/celeryJuice.jpg'><img style='height: 175px;' src='./img/celeryJuice.jpg' alt='celeryJuice'></a>
+                  <div class='caption'>
+                    <h4><a width='150' height='350' href='./img/celeryJuice.jpg'>celery Juice</a></h4>
+                    <p>celery Juice</p>
+                  </div>
+                </div>  
+              </div>
+              
+              <div class='col-sm-4 col-lg-4 col-md-4'>
+                <div class='thumbnail'>
+                  <a href='./img/beetJuice.jpg'><img style='height: 175px;' src='./img/beetJuice.jpg' alt='beetJuice'></a>
+                  <div class='caption'>
+                    <h4><a width='150' height='350' href='./img/beetJuice.jpg'>beet Juice</a></h4>
+                    <p>beet Juice</p>
+                  </div>
+                </div>  
+              </div>
+              
             </div>
             
             
@@ -38,10 +70,10 @@ echo "<header class='masthead'>
               <h1>Shots</h1>
               <div class='col-sm-4 col-lg-4 col-md-4'>
                 <div class='thumbnail'>
-                  <a href='./img/appleBlueberrySmoothie1.jpg'><img style='height: 175px;' src='./img/appleBlueberrySmoothie.jpg' alt='appleBlueberrySmoothie'></a>
+                  <a href='./img/gingerShot.jpg'><img style='height: 175px;' src='./img/gingerShot.jpg' alt='gingerShot'></a>
                   <div class='caption'>
-                    <h4><a width='150' height='350' href='./img/appleBlueberrySmoothie1.jpg'>Apple, Blueberry, and Banana</a></h4>
-                    <p>Apple, Blueberry, and Banana Smoothie</p>
+                    <h4><a width='150' height='350' href='./img/gingerShot.jpg'>shot of ginger</a></h4>
+                    <p>shot of ginger</p>
                   </div>
                 </div>  
               </div>
