25bd273e67f7393d2d386146cf892176f5c21e54
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-07 01:04:32 -0500

    Update graphicDesign.php

--- app/services/graphicDesign/php/graphicDesign.php
+++ app/services/graphicDesign/php/graphicDesign.php
@@ -1,8 +1,15 @@
 <?php
+
+$path    = './img/logos';
+$files = scandir($path);
+
 echo "<header class='masthead'>
         <div class='container d-flex h-100 align-items-center'>
           <h1>coming Soon!!!</h1>
           </div>
         </div>
-      </header>";
+      </header>
+      <script>console.log({$files});</script>
+      
+      ";
 ?>
