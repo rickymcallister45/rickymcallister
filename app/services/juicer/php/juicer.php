d4b342e2dbb6ba1aeeebbe1f84ec7dfa2f781793
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-06 23:52:21 -0500

    Update juicer.php

--- app/services/juicer/php/juicer.php
+++ app/services/juicer/php/juicer.php
@@ -3,9 +3,51 @@ echo "<header class='masthead'>
         <div class='container d-flex h-100 align-items-center'>
           <div class='pageContent'>
           
-            <a href='./img/appleBlueberrySmoothie1.jpg'><img src='./img/appleBlueberrySmoothie.jpg'>
-            </a>
-          
+          <div class=row>
+            <h1>Smooties</h1>
+              <div class='col-sm-4 col-lg-4 col-md-4'>
+                <div class='thumbnail'>
+                  <a href='./img/appleBlueberrySmoothie1.jpg'><img style='height: 175px;' src='./img/appleBlueberrySmoothie.jpg' alt='appleBlueberrySmoothie'></a>
+                  <div class='caption'>
+                    <h4><a width='150' height='350' href='./img/appleBlueberrySmoothie1.jpg'>Apple, Blueberry, and Banana</a></h4>
+                    <p>Apple, Blueberry, and Banana Smoothie</p>
+                  </div>
+                </div>  
+              </div>
+            </div>
+            
+            
+            
+            <div class=row>
+              <h1>Juices</h1>
+              <div class='col-sm-4 col-lg-4 col-md-4'>
+                <div class='thumbnail'>
+                  <a href='./img/appleBlueberrySmoothie1.jpg'><img style='height: 175px;' src='./img/appleBlueberrySmoothie.jpg' alt='appleBlueberrySmoothie'></a>
+                  <div class='caption'>
+                    <h4><a width='150' height='350' href='./img/appleBlueberrySmoothie1.jpg'>Apple, Blueberry, and Banana</a></h4>
+                    <p>Apple, Blueberry, and Banana Smoothie</p>
+                  </div>
+                </div>  
+              </div>
+            </div>
+            
+            
+            
+            
+            <div class=row>
+              <h1>Shots</h1>
+              <div class='col-sm-4 col-lg-4 col-md-4'>
+                <div class='thumbnail'>
+                  <a href='./img/appleBlueberrySmoothie1.jpg'><img style='height: 175px;' src='./img/appleBlueberrySmoothie.jpg' alt='appleBlueberrySmoothie'></a>
+                  <div class='caption'>
+                    <h4><a width='150' height='350' href='./img/appleBlueberrySmoothie1.jpg'>Apple, Blueberry, and Banana</a></h4>
+                    <p>Apple, Blueberry, and Banana Smoothie</p>
+                  </div>
+                </div>  
+              </div>
+            </div>
+            
+                        
           </div>
         </div>
       </header>";
