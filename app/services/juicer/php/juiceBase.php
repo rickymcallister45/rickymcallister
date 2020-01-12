fd52708c1bf39dcb07bd5a50c1bba903b09720d1
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-12 03:05:09 -0500

    Create juiceBase.php

---
+++ app/services/juicer/php/juiceBase.php
@@ -0,0 +1,51 @@
+<?php
+echo "<header class='masthead'>
+        <div class='container d-flex h-100 align-items-center'>
+          <div class='pageContent'>
+          
+          <div class=row>
+            <div class='col-sm-4 col-lg-4 col-md-4 bgwhite'>
+                <div class='thumbnail'>
+                  <a href='/index.php?menu'><img style='height: 175px;' src='./img/selector/menu/juiceMenu.jpg' alt='Juice Bar Menu'></a>
+                  <div class='caption'>
+                    <h4><a width='150' height='350' href='/index.php?menu'>Menu</a></h4>
+                    <p>Juice Bar Menu</p>
+                  </div>
+                </div>  
+              </div>
+              
+             <div class='col-sm-4 col-lg-4 col-md-4 bgwhite'>
+                <div class='thumbnail'>
+                  <a href='/index.php?juices'><img style='height: 175px;' src='./img/selector/menu/juices.jpg' alt='Juice options'></a>
+                  <div class='caption'>
+                    <h4><a width='150' height='350' href='/index.php?juices'>Juices</a></h4>
+                    <p>Fresh pressed juices</p>
+                  </div>
+                </div>  
+              </div>
+            
+           <div class='col-sm-4 col-lg-4 col-md-4 bgwhite'>
+                <div class='thumbnail'>
+                  <a href='/index.php?smoothies'><img style='height: 175px;' src='./img/selector/menu/smoothies.jpg' alt='Smoothie options'></a>
+                  <div class='caption'>
+                    <h4><a width='150' height='350' href='/index.php?smoothies'>Menu</a></h4>
+                    <p>Blended Smooties</p>
+                  </div>
+                </div>  
+              </div>
+              
+              <div class='col-sm-4 col-lg-4 col-md-4 bgwhite'>
+                <div class='thumbnail'>
+                  <a href='/index.php?shots'><img style='height: 175px;' src='./img/selector/menu/shots.jpg' alt='Shot options'></a>
+                  <div class='caption'>
+                    <h4><a width='150' height='350' href='/index.php?shots'>Shots</a></h4>
+                    <p>Power Shots</p>
+                  </div>
+                </div>  
+              </div>
+            
+                        
+          </div>
+        </div>
+      </header>";
+?>
