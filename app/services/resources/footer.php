cfc51eb9db8d2156ae20200da32de7b9dccdd4d6
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-13 00:38:18 -0500

    Update footer.php

--- app/services/resources/footer.php
+++ app/services/resources/footer.php
@@ -4,12 +4,16 @@ echo "<footer class='bg-black small text-center text-white-50'>
       Copyright &copy; rickyMcAllister.com 2019
     </div>
   </footer>
-
+  <div>
   <script src='../js/jquery.min.js'></script>
   <script src='../js/bootstrap.bundle.min.js'></script>
   
   <script src='../js/jquery.easing.min.js'></script>
 
   <script src='../js/grayscale.min.js'></script>
-  <script src='./js/script.js'></script>";
+  <script src='./js/script.js'></script>
+  <script src='https://kit.fontawesome.com/8afd1db41f.js'></script>
+  </div>";
+
+
 ?>
