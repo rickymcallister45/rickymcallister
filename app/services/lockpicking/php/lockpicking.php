4a264fb8a5013490c9ba6d0a99f2e9ff44fd6a75
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-12 22:23:26 -0500

    Update lockpicking.php

--- app/services/lockpicking/php/lockpicking.php
+++ app/services/lockpicking/php/lockpicking.php
@@ -1,8 +1,7 @@
 <?php
-echo "<header class='masthead'>
+echo "<header class='masthead'></header>
         <div class='container d-flex h-100 align-items-center'>
           <div class='lockpickingVideos'>    
           </div>
-        </div>
-      </header>";
+        </div>";
 ?>
