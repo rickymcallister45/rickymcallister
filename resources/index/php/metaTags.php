ed7dfff72cee1a808d30c801e2ddab235967b13e
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-14 03:05:29 -0500

    Create metaTags.php

---
+++ resources/index/php/metaTags.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "<meta charset='UTF-8'>
+      <meta name='keywords' content='Ricky McAllister, McAllister'/>
+      <meta name='description' content='The real Ricky McAllisters personal website'/>
+      <meta name='subject' content='Ricky McAllisters personal website'>
+      <meta name='copyright'content='Ricky McAllister'>
+      <meta name='language' content='JavaScript, PHP, mySql, Python'>
+      <meta name='robots' content='index,follow'/>
+      <meta name='abstract' content='The real Ricky McAllisters personal website'>
+      <meta name='topic' content='Personal Portfolio'>
+      <meta name='summary' content='Ricky McAllisters personal Portfolio'>
+      <meta name='Classification' content='Business'>
+      <meta name='author' content='Ricky McAllister, rickymcallister45@gmail.com'>
+      <meta name='designer' content='Ricky McAllister'>
+      <meta name='copyright' content='Ricky McAllister 2017'>
+      <meta name='reply-to' content='rickymcallister45@gmail.com'>
+      <meta name='owner' content='Ricky McAllister'>
+      <meta name='url' content='http://rickymcallister.com/'>
+      <meta name='identifier-URL' content='http://rickymcallister.com/'>";
+      
+?>
