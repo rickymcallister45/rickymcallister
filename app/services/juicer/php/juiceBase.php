18088c59ee2f4d2be00e4971f19814e193495cc4
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-15 01:06:46 -0500

    Update juiceBase.php

--- app/services/juicer/php/juiceBase.php
+++ app/services/juicer/php/juiceBase.php
@@ -6,9 +6,9 @@ echo "<header class='masthead'></header>
           <div class=row>
             <div class='col-sm-4 col-lg-4 col-md-4 bgwhite'>
                 <div class='thumbnail'>
-                  <a href='/index.php?menu'><img class='cardImage' src='./img/selector/juiceMenu.jpg' alt='Juice Bar Menu'></a>
+                  <a href='/app/services/juicer/index.php?menu'><img class='cardImage' src='./img/selector/juiceMenu.jpg' alt='Juice Bar Menu'></a>
                   <div class='caption'>
-                    <h4><a width='150' height='350' href='/index.php?menu'>Menu</a></h4>
+                    <h4><a width='150' height='350' href='/app/services/juicer/index.php?menu'>Menu</a></h4>
                     <p>Juice Bar Menu</p>
                   </div>
                 </div>  
@@ -16,9 +16,9 @@ echo "<header class='masthead'></header>
               
              <div class='col-sm-4 col-lg-4 col-md-4 bgwhite'>
                 <div class='thumbnail'>
-                  <a href='/index.php?juices'><img class='cardImage' src='./img/selector/juices.jpg' alt='Juice options'></a>
+                  <a href='/app/services/juicer/index.php?juices'><img class='cardImage' src='./img/selector/juices.jpg' alt='Juice options'></a>
                   <div class='caption'>
-                    <h4><a width='150' height='350' href='/index.php?juices'>Juices</a></h4>
+                    <h4><a width='150' height='350' href='/app/services/juicer/index.php?juices'>Juices</a></h4>
                     <p>Fresh pressed juices</p>
                   </div>
                 </div>  
@@ -26,9 +26,9 @@ echo "<header class='masthead'></header>
             
            <div class='col-sm-4 col-lg-4 col-md-4 bgwhite'>
                 <div class='thumbnail'>
-                  <a href='/index.php?smoothies'><img class='cardImage' src='./img/selector/smoothies.jpg' alt='Smoothie options'></a>
+                  <a href='/app/services/juicer/index.php?smoothies'><img class='cardImage' src='./img/selector/smoothies.jpg' alt='Smoothie options'></a>
                   <div class='caption'>
-                    <h4><a width='150' height='350' href='/index.php?smoothies'>Smoothies</a></h4>
+                    <h4><a width='150' height='350' href='/app/services/juicer/index.php?smoothies'>Smoothies</a></h4>
                     <p>Blended Smooties</p>
                   </div>
                 </div>  
@@ -36,9 +36,9 @@ echo "<header class='masthead'></header>
               
               <div class='col-sm-4 col-lg-4 col-md-4 bgwhite'>
                 <div class='thumbnail'>
-                  <a href='/index.php?shots'><img class='cardImage' src='./img/selector/shots.jpg' alt='Shot options'></a>
+                  <a href='/app/services/juicer/index.php?shots'><img class='cardImage' src='./img/selector/shots.jpg' alt='Shot options'></a>
                   <div class='caption'>
-                    <h4><a width='150' height='350' href='/index.php?shots'>Shots</a></h4>
+                    <h4><a width='150' height='350' href='/app/services/juicer/index.php?shots'>Shots</a></h4>
                     <p>Power Shots</p>
                   </div>
                 </div>  
