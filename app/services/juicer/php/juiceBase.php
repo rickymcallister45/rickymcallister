c377cf22a32ac77abe6ffe58c4c1275178ce12bc
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-14 22:51:30 -0500

    Update juiceBase.php

--- app/services/juicer/php/juiceBase.php
+++ app/services/juicer/php/juiceBase.php
@@ -6,7 +6,7 @@ echo "<header class='masthead'></header>
           <div class=row>
             <div class='col-sm-4 col-lg-4 col-md-4 bgwhite'>
                 <div class='thumbnail'>
-                  <a href='/index.php?menu'><img style='height: 175px;' src='./img/selector/juiceMenu.jpg' alt='Juice Bar Menu'></a>
+                  <a href='/index.php?menu'><img class='cardImage' src='./img/selector/juiceMenu.jpg' alt='Juice Bar Menu'></a>
                   <div class='caption'>
                     <h4><a width='150' height='350' href='/index.php?menu'>Menu</a></h4>
                     <p>Juice Bar Menu</p>
@@ -16,7 +16,7 @@ echo "<header class='masthead'></header>
               
              <div class='col-sm-4 col-lg-4 col-md-4 bgwhite'>
                 <div class='thumbnail'>
-                  <a href='/index.php?juices'><img style='height: 175px;' src='./img/selector/juices.jpg' alt='Juice options'></a>
+                  <a href='/index.php?juices'><img class='cardImage' src='./img/selector/juices.jpg' alt='Juice options'></a>
                   <div class='caption'>
                     <h4><a width='150' height='350' href='/index.php?juices'>Juices</a></h4>
                     <p>Fresh pressed juices</p>
@@ -26,7 +26,7 @@ echo "<header class='masthead'></header>
             
            <div class='col-sm-4 col-lg-4 col-md-4 bgwhite'>
                 <div class='thumbnail'>
-                  <a href='/index.php?smoothies'><img style='height: 175px;' src='./img/selector/smoothies.jpg' alt='Smoothie options'></a>
+                  <a href='/index.php?smoothies'><img class='cardImage' src='./img/selector/smoothies.jpg' alt='Smoothie options'></a>
                   <div class='caption'>
                     <h4><a width='150' height='350' href='/index.php?smoothies'>Menu</a></h4>
                     <p>Blended Smooties</p>
@@ -36,7 +36,7 @@ echo "<header class='masthead'></header>
               
               <div class='col-sm-4 col-lg-4 col-md-4 bgwhite'>
                 <div class='thumbnail'>
-                  <a href='/index.php?shots'><img style='height: 175px;' src='./img/selector/shots.jpg' alt='Shot options'></a>
+                  <a href='/index.php?shots'><img class='cardImage' src='./img/selector/shots.jpg' alt='Shot options'></a>
                   <div class='caption'>
                     <h4><a width='150' height='350' href='/index.php?shots'>Shots</a></h4>
                     <p>Power Shots</p>
