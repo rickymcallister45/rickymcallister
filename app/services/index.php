a896c74efd0b1fdb99890d7f45ae5e189fd4c227
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-21 02:09:45 -0500

    Update index.php

--- app/services/index.php
+++ app/services/index.php
@@ -17,6 +17,7 @@ echo "<!DOCTYPE html>
 
  <link href='./css/bootstrap.min.css' rel='stylesheet'>
  <link href='./css/business-frontpage.css' rel='stylesheet'>
+ <link href='./css/style.css' rel='stylesheet'>
 
 </head>
 
