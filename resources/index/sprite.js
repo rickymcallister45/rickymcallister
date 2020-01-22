2c8a5a6e5486b934c15054673d062b8015a2f7ef
Ricky McAllister <rickymcallister45@gmail.com>
2020-01-21 22:21:53 -0500

    Update sprite.js

--- resources/index/sprite.js
+++ resources/index/sprite.js
@@ -6,80 +6,80 @@ let data = [{
 },
 {
   title:'calender',
-  x: -200,
+  x: -50,
   y: 0,
   path: 'https://calendar.google.com/'
 },
 {
   title:'clock',
-  x: -400,
+  x: -100,
   y: 0,
   path: 'needsFiguredOut'
   },
 {
   title:'gmail',
-  x: -600,
+  x: -150,
   y: 0,
   path: 'https://mail.google.com/'
   },
 {
   title:'entertainment',
   x: 0,
-  y: -205,
+  y: -50,
   path: 'http://rickymcallister.com/app/entertainment/r6index.html'
   },
 {
   title:'grocery',
-  x: -200,
-  y: -200,
+  x: -50,
+  y: -50,
   path: 'http://rickymcallister.com/app/groceryList/index.php'
   },
 {
   title:'keep',
-  x: -400,
-  y: -200,
+  x: -100,
+  y: -50,
   path: 'https://keep.google.com/'
   },
 {
   title:'controller',
-  x: -600,
-  y: -200,
+  x: -150,
+  y: -50,
   path: 'http://rickymcallister.com/app/controller/index.html'
   },
 {
   title:'store',
   x: 0,
-  y: -400,
+  y: -100,
   path: 'http://rickymcallister.com/app/store/index.php'
 },
 {
   title:'soundboard',
-  x: -200,
-  y: -400,
+  x: -50,
+  y: -100,
   path: 'http://rickymcallister.com/app/soundboard/index.php'
 },
 {
   title:'services',
-  x: -400,
-  y: -400,
+  x: -100,
+  y: -100,
   path: 'http://rickymcallister.com/app/services/index.php'
 },
 {
   title:'radar',
-  x: -600,
-  y: -400,
+  x: -150,
+  y: -100,
   path: 'http://rickymcallister.com/app/radar/index.html'
 },
 {
   title:'todo',
   x: 0,
-  y: -600,
+  y: -150,
   path: 'http://rickymcallister.com/app/toDo/index.php'
 },
 {
   title:'tv',
-  x: -200,
-  y: -600,
+  x: -50,
+  y: -150,
   path: 'http://rickymcallister.com/app/remote/index.html'
 },
 
