9aaebab80dcc52f1ff0fc7c4530ac8b5ffb3ca19
Ricky McAllister <rickymcallister45@gmail.com>
2020-02-02 02:13:17 -0500

    Create meta.php

---
+++ resources/index/php/meta.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "<!DOCTYPE html>
+        <html>
+        <head>
+	        <meta charset='UTF-8'>
+	        <meta name='viewport' content='width=device-width, initial-scale=1'>
+	        <title>Ricky McAllister</title>";
+?>
