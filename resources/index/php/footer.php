c64775626f39dff273d814585b6e6d6be74c412c
Ricky McAllister <rickymcallister45@gmail.com>
2020-02-02 02:18:23 -0500

    Create footer.php

---
+++ resources/index/php/footer.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "<footer>
+	      <script  src='https://code.jquery.com/jquery-3.3.1.min.js'></script>
+	      <script type='text/javascript' src='./resources/index/script.min.js'></script>
+	      <script type='text/javascript' src='./resources/index/sprite.js'></script>
+	    </footer>";
+?>
