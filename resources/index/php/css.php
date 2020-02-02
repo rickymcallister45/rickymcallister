934c35abfb4aa0414602300ed865f73b6358c27d
Ricky McAllister <rickymcallister45@gmail.com>
2020-02-02 02:15:24 -0500

    Create css.php

---
+++ resources/index/php/css.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "<link rel='icon' href='./resources/favicon/favicon.ico' type='image/x-icon'>
+	    <link href='https://use.fontawesome.com/releases/v5.8.1/css/all.css' rel='stylesheet'>
+	    <link rel='stylesheet' type='text/css' href='./resources/index/style.min.css'>
+    </head>";
+
+?>
