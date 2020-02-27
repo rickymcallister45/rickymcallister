1296f3eff6be17ee13ee06a92cc773c644641c13
Ricky McAllister <rickymcallister45@gmail.com>
2020-02-27 02:06:28 -0500

    Update vanillaBackground.js

--- app/desktop/js/vanillaBackground.js
+++ app/desktop/js/vanillaBackground.js
@@ -1,18 +1,23 @@
-function background(){
-var request = new XMLHttpRequest();
-var url = 'https://www.reddit.com/r/';
-var differnetSites = ['roomporn', 'CozyPlaces', 'MostBeautiful', 'pic', 'carporn', 'AbandonedPorn', 'EarthPorn', 'SkyPorn', 'astrophotography', 'spaceporn', 'itookapicture', 'Eyebleach'];
+let request = new XMLHttpRequest();
+let url = 'https://www.reddit.com/r/';
+let differnetSites = ['roomporn', 'CozyPlaces', 'MostBeautiful', 'pic', 'carporn', 'AbandonedPorn', 'EarthPorn', 'SkyPorn', 'astrophotography', 'spaceporn', 'itookapicture', 'Eyebleach'];
+let i = Math.floor(Math.random() * 11) + 1;
+let randomNumberToSelectSite = Math.floor(Math.random() * 11);
 
-var randomNumberToSelectSite = Math.floor(Math.random() * 11);
-var siteTheBackroundImageIsComingFrom = differnetSites[randomNumberToSelectSite];
+
+function background(){
+let siteTheBackroundImageIsComingFrom = differnetSites[randomNumberToSelectSite];
 console.log('siteTheBackroundImageIsComingFrom = ' + url + siteTheBackroundImageIsComingFrom);
 
 
-var backgroundURL; 
+let backgroundURL;
+    
     request.open('GET', url + siteTheBackroundImageIsComingFrom + '.json', true);
     request.onload = function() {
-          var data = JSON.parse(request.responseText);
-          var i = Math.floor(Math.random() * 11) + 1;
+          let data = JSON.parse(request.responseText);
+          
+          //i = Math.floor(Math.random() * 11) + 1;
+          
           backgroundURL = (data.data.children[i].data.url);
           document.body.style.backgroundImage = "url(" + backgroundURL + ")";
           
@@ -21,3 +26,36 @@ var backgroundURL;
 }
 
 background();
+
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 37:
+            randomNumberToSelectSite--;
+            if(randomNumberToSelectSite < 0){
+              randomNumberToSelectSite = 11;
+            }
+            background();
+            break;
+        case 38:
+            i++;
+            if(i > 24){
+              i = 2;
+            }
+            background();
+            break;
+        case 39:
+            randomNumberToSelectSite++;
+            if(randomNumberToSelectSite > differnetSites.length-1){
+              randomNumberToSelectSite = 0;
+            }
+            background();
+            break;
+        case 40:
+            i--;
+            if(i < 2){
+              i = 24;
+            }
+            background();
+            break;
+    }
+};
