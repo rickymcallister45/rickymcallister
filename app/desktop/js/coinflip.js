6f2f81ace9a5ade25ad1da7bf5c7a0707a66ccd8
Ricky McAllister <rickymcallister45@gmail.com>
2020-03-05 01:24:38 -0500

    Update coinflip.js

--- app/desktop/js/coinflip.js
+++ app/desktop/js/coinflip.js
@@ -1,41 +1,57 @@
-var p5;
+function coinFlip(){
 
-var coinFlip = function(cf){
+var timer = 2000;
 
-var time = 2000;
-var heads = "img/heads.png";
-var tails = "img/tails.png";
-var number;
-var answer;
+var number =  Math.floor((Math.random() * 2) + 1);
 
-cf.decision = function(number) {
-			if (number == 1) {
-				console.log("heads");
-				answer = cf.createImg(heads);
-				} else {
-				console.log("tails");
-				answer = cf.createImg(tails);
-			}	}	
-
-cf.keyTyped =	function(){
-	if (cf.keyCode === 32) { 
-		
-		console.log("coinFlip Starting!!");
-		var number =  Math.floor((Math.random() * 2) + 1)
-		console.log(number);
- 
-		setInterval(cf.timeIt, time);
-		cf.decision(number);
-		answer.position(825,225);
-		}else{
-		return(false);	
-
-}	}
-
-cf.timeIt = function() {
-		answer.remove();
-	}
-cf.draw = function(){}
+function hideCoin() {
+  var coinArray = 0;
+  hideThisCoin = document.getElementById("coin").remove();
 }
 
-var myp5 = new p5(coinFlip);
+function heads() {
+  var heads = document.createElement("IMG");
+  heads.setAttribute("src", "img/heads.png");
+  heads.setAttribute("width", "200");
+  heads.setAttribute("height", "200");
+  heads.setAttribute("id", "coin");
+  heads.style.visibility = "visible";
+  heads.style.position = "absolute";
+  heads.style.top = "35vh";
+  heads.style.left = "45vw";
+  document.body.appendChild(heads);
+}
+
+function tails() {
+  var tails = document.createElement("IMG");
+  tails.setAttribute("src", "img/tails.png");
+  tails.setAttribute("width", "200");
+  tails.setAttribute("height", "200");
+  tails.style.visibility = "visible";
+  tails.style.position = "absolute";
+  tails.style.top = "35vh";
+  tails.style.left = "45vw";
+  tails.setAttribute("id", "coin");
+  document.body.appendChild(tails);
+}
+
+function decision(number) {
+			if (number == 1) {
+			  console.log("heads");
+				heads();
+				} else {
+				console.log("tails");
+				tails();
+			  }
+      }
+      
+decision(number);
+setTimeout(hideCoin, timer);
+}
+
+document.onkeydown = function(e) {
+  if (e.keyCode === 32) {
+    coinFlip();
+    
+  }
+};
