a17a6dbc4f56f02b0787ecc016170759ef6cc960
Ricky McAllister <rickymcallister45@gmail.com>
2020-03-06 00:02:05 -0500

    Update coinflip.js

--- app/desktop/js/coinflip.js
+++ app/desktop/js/coinflip.js
@@ -50,8 +50,37 @@ setTimeout(hideCoin, timer);
 }
 
 document.onkeydown = function(e) {
-  if (e.keyCode === 32) {
-    coinFlip();
-    
-  }
+  switch (e.keyCode) {
+        case 37:
+            randomNumberToSelectSite--;
+            if(randomNumberToSelectSite < 0){
+              randomNumberToSelectSite = differnetSites.length;
+            }
+            background();
+            break;
+        case 38:
+            randomPicture++;
+            if(randomPicture > 24){
+              randomPicture = 2;
+            }
+            background();
+            break;
+        case 39:
+            randomNumberToSelectSite++;
+            if(randomNumberToSelectSite > differnetSites.length - 1){
+              randomNumberToSelectSite = 0;
+            }
+            background();
+            break;
+        case 40:
+            randomPicture--;
+            if(randomPicture < 2){
+              randomPicture = 24;
+            }
+            background();
+            break;
+        case 32:
+            coinFlip();
+            break;
+    }  
 };
