adffacc757740b2b282c889c945fe663f417eb75
Ricky McAllister <rickymcallister45@gmail.com>
2020-03-06 05:45:30 -0500

    Update searchBar.js

--- app/desktop/js/searchBar.js
+++ app/desktop/js/searchBar.js
@@ -1,13 +1,29 @@
-//*searchBar
-	$('#search').click(function(){
-		setTimeout(function(){ $('#searchFocus').focus(); }, 600);
-		$('.menu-item').toggleClass('hide-search')
-		$('.search-form').toggleClass('searchActive')
-	})
-	$( "input" ).keyup(function(enter) {
-    var value = $( this ).val();
-    console.log(value);
-    if (enter.keyCode == 13) {
-    	window.open('http://www.google.com/search?q=' + value + '&btnI');
-    }
-	})
+function searchBar(){
+  
+  var searchbar = document.createElement("INPUT");
+  searchbar.setAttribute("type", "text");
+  searchbar.setAttribute("id", "searchbar");
+  searchbar.setAttribute("autofocus", '');
+  searchbar.style.width = "800px";
+  searchbar.style.height = "400px";
+  searchbar.style.position = "absolute";
+  searchbar.style.top = "25vh";
+  searchbar.style.left = "25vw";
+  searchbar.style.fontSize = "100px";
+  searchbar.style.textAlign = "center";
+  
+  document.body.appendChild(searchbar);
+  
+}
+
+
+function hideSearchBar() {
+    var hideSearchBar = document.getElementById("searchbar").remove();
+  }
+  
+  function runSearch(){
+    var inputVal = document.getElementById("searchbar").value;
+    window.open('http://www.google.com/search?q=' + inputVal + '&btnI');
+    setTimeout(hideSearchBar, 1000);
+    
+  }
