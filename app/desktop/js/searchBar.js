3f00a0d8d9166e7eac06d88541668a57ca6b64cc
Ricky McAllister <rickymcallister45@gmail.com>
2020-04-11 23:09:25 -0400

    Update searchBar.js

--- app/desktop/js/searchBar.js
+++ app/desktop/js/searchBar.js
@@ -1,29 +1,5 @@
-function searchBar(){
-  
-  var searchbar = document.createElement("INPUT");
-  searchbar.setAttribute("type", "text");
-  searchbar.setAttribute("id", "searchbar");
-  searchbar.setAttribute("autofocus", '');
-  searchbar.style.width = "800px";
-  searchbar.style.height = "400px";
-  searchbar.style.position = "absolute";
-  searchbar.style.top = "25vh";
-  searchbar.style.left = "25vw";
-  searchbar.style.fontSize = "100px";
-  searchbar.style.textAlign = "center";
-  
-  document.body.appendChild(searchbar);
-  
-}
-
-
-function hideSearchBar() {
-    var hideSearchBar = document.getElementById("searchbar").remove();
-  }
-  
-  function runSearch(){
-    var inputVal = document.getElementById("searchbar").value;
-    window.open('http://www.google.com/search?q=' + inputVal + '&btnI');
-    setTimeout(hideSearchBar, 1000);
-    
-  }
+function searchBar() {
+  var search = prompt("Please enter your query", "");
+  if (search !== null) {
+    window.open('http://www.google.com/search?q=' + search + '&btnI');
+  }}
